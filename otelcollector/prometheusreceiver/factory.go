b348841237e69ec53ec06f8eb66a929e7d07981e
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-08-09 21:39:22 -0700

    changes

--- otelcollector/prometheusreceiver/factory.go
+++ otelcollector/prometheusreceiver/factory.go
@@ -24,6 +24,7 @@ import (
 	"go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver/receiverhelper"
+	"github.com/gracewehner/prometheusreceiver/internal"
 )
 
 // This file implements config for Prometheus receiver.
@@ -47,6 +48,7 @@ func NewFactory() component.ReceiverFactory {
 func createDefaultConfig() config.Receiver {
 	return &Config{
 		ReceiverSettings: config.NewReceiverSettings(config.NewID(typeStr)),
+		logger := internal.NewZapToGokitLogAdapter(config.logger),
 	}
 }
 
