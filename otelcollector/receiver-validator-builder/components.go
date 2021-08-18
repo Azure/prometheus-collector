b433d9d4ced86b6576f758a23758a5d4e66ee657
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-08-18 11:12:25 -0700

    changes

--- otelcollector/receiver-validator-builder/components.go
+++ otelcollector/receiver-validator-builder/components.go
@@ -1,21 +1,21 @@
-package main
+// package main
 
-import (
-	"go.opentelemetry.io/collector/component"
+// import (
+// 	"go.opentelemetry.io/collector/component"
 
-	//"github.com/vishiy/influxexporter"
-	//"go.opentelemetry.io/collector/receiver/prometheusreceiver"
-	privatepromreceiver "github.com/gracewehner/prometheusreceiver"
-)
+// 	//"github.com/vishiy/influxexporter"
+// 	//"go.opentelemetry.io/collector/receiver/prometheusreceiver"
+// 	privatepromreceiver "github.com/gracewehner/prometheusreceiver"
+// )
 
-func components() component.ReceiverFactory {
-	// var errs []error
+// func components() component.ReceiverFactory {
+// 	// var errs []error
 
-	receiver := privatepromreceiver.NewFactory()
+// 	receiver := privatepromreceiver.NewFactory()
 
-	// if err != nil {
-	// 	errs = append(errs, err)
-	// }
+// 	// if err != nil {
+// 	// 	errs = append(errs, err)
+// 	// }
 
-	return receiver
-}
+// 	return receiver
+// }
--- otelcollector/receiver-validator-builder/main.go
+++ otelcollector/receiver-validator-builder/main.go
@@ -10,35 +10,29 @@ import (
 )
 
 func main() {
-	receiver := components()
+	// receiver := components()
+	// receiver := privatepromreceiver.NewFactory()
 	configFilePtr := flag.String("config-file", "", "Config file to validate")
 	flag.Parse()
 	filePath := *configFilePtr
 	fmt.Printf("Config file provided : %s", filePath)
-	// info := component.BuildInfo{
-	// 	Command:     "custom-receiver-validator",
-	// 	Description: "Custom Receiver validator",
-	// 	Version:     "1.0.0",
-	// }
-	// fmt.Printf("Receiver: %v", receiver)
-	fmt.Printf("Receiver: %+v\n", receiver)
-	defCfg := receiver.CreateDefaultConfig()
-	fmt.Printf("DefConfig: %+v\n", defCfg)
+
+	// fmt.Printf("Receiver: %+v\n", receiver)
+	// defCfg := receiver.CreateDefaultConfig()
+	// fmt.Printf("DefConfig: %+v\n", defCfg)
 
 	configContents, err := promconfig.LoadFile(filePath, false, log.NewNopLogger())
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
-	fmt.Printf("Config contents: %v", configContents)
+	// fmt.Printf("Config contents: %v", configContents)
 
-	//customConfig := receiver.createCustomConfig(configContents)
-	customConfig := &privatepromreceiver.Config{
+	receiverConfig := &privatepromreceiver.Config{
 		PrometheusConfig: configContents,
 	}
-	fmt.Printf("CustomConfig: %+v\n", customConfig)
-	//cfg.PrometheusConfig = configContents
+	fmt.Printf("ReceiverConfig: %+v\n", receiverConfig)
 
-	cfgErr := customConfig.Validate()
+	cfgErr := receiverConfig.Validate()
 	if cfgErr != nil {
 		fmt.Printf("Error: %v", cfgErr)
 	}
