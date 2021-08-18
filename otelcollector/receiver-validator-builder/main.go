a35cc0f2c95fafaa6d36c9249749286c1820fe23
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-08-18 11:15:29 -0700

    cleaning up

--- otelcollector/receiver-validator-builder/main.go
+++ otelcollector/receiver-validator-builder/main.go
@@ -10,27 +10,20 @@ import (
 )
 
 func main() {
-	// receiver := components()
-	// receiver := privatepromreceiver.NewFactory()
 	configFilePtr := flag.String("config-file", "", "Config file to validate")
 	flag.Parse()
 	filePath := *configFilePtr
-	fmt.Printf("Config file provided : %s", filePath)
-
-	// fmt.Printf("Receiver: %+v\n", receiver)
-	// defCfg := receiver.CreateDefaultConfig()
-	// fmt.Printf("DefConfig: %+v\n", defCfg)
+	fmt.Printf("Config file provided : %s \n", filePath)
 
 	configContents, err := promconfig.LoadFile(filePath, false, log.NewNopLogger())
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
-	// fmt.Printf("Config contents: %v", configContents)
 
 	receiverConfig := &privatepromreceiver.Config{
 		PrometheusConfig: configContents,
 	}
-	fmt.Printf("ReceiverConfig: %+v\n", receiverConfig)
+	// fmt.Printf("ReceiverConfig: %+v\n", receiverConfig)
 
 	cfgErr := receiverConfig.Validate()
 	if cfgErr != nil {
