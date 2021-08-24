9e7214eb5a3582141a7699a594bf65b0b0a9b799
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-08-24 15:52:58 -0700

    changes

--- otelcollector/receiver-config-validator-builder/main.go
+++ otelcollector/receiver-config-validator-builder/main.go
@@ -7,33 +7,39 @@ import (
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/service"
+	"go.opentelemetry.io/collector/service/parserprovider"
 )
 
 func main() {
 
-	factories, err := components()
-	if err != nil {
-		log.Fatalf("failed to build components: %v", err)
-	}
-	info := component.BuildInfo{
-		Command:     "custom-collector-distro",
-		Description: "Custom OpenTelemetry Collector distributionr",
-		Version:     "1.0.0",
-	}
+	parserProvider parserprovider.ParserProvider
+	def := parserProvider.Default()
 
-	col, err := service.New(service.Parameters{BuildInfo: info, Factories: factories})
-	if err != nil {
-		log.Fatal("failed to construct the collector server: %w", err)
-	}
+	fmt.Printf("def parser provider: %v", def)
 
-	fmt.Printf("Loading configuration...")
+	// factories, err := components()
+	// if err != nil {
+	// 	log.Fatalf("failed to build components: %v", err)
+	// }
+	// info := component.BuildInfo{
+	// 	Command:     "custom-collector-distro",
+	// 	Description: "Custom OpenTelemetry Collector distributionr",
+	// 	Version:     "1.0.0",
+	// }
 
-	cp, err := col.parserProvider.Get()
-	if err != nil {
-		return fmt.Errorf("cannot load configuration's parser: %w", err)
-	}
+	// col, err := service.New(service.Parameters{BuildInfo: info, Factories: factories})
+	// if err != nil {
+	// 	log.Fatal("failed to construct the collector server: %w", err)
+	// }
 
-	fmt.Printf("parser provider - %v", cp)
+	// fmt.Printf("Loading configuration...")
+
+	// cp, err := col.parserProvider.Get()
+	// if err != nil {
+	// 	return fmt.Errorf("cannot load configuration's parser: %w", err)
+	// }
+
+	// fmt.Printf("parser provider - %v", cp)
 
 	// configFilePtr := flag.String("config-file", "", "Config file to validate")
 	// flag.Parse()
