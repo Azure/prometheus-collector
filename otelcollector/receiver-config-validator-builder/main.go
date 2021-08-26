94d6e81fe3e3e466aab77d2077e18e25b686d2e0
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-08-26 12:03:02 -0700

    changes

--- otelcollector/receiver-config-validator-builder/main.go
+++ otelcollector/receiver-config-validator-builder/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"go.opentelemetry.io/collector/config/configloader"
 	parserProvider "go.opentelemetry.io/collector/service/parserprovider"
@@ -33,11 +34,13 @@ func main() {
 		})
 		if err != nil {
 			fmt.Printf("Error parsing flags - %v\n", err)
+			os.Exit(1)
 		}
 
 		factories, err := components()
 		if err != nil {
 			log.Fatalf("failed to build components: %v\n", err)
+			os.Exit(1)
 		}
 
 		colParserProvider := parserProvider.Default()
@@ -45,17 +48,20 @@ func main() {
 		cp, err := colParserProvider.Get()
 		if err != nil {
 			fmt.Errorf("cannot load configuration's parser: %w\n", err)
+			os.Exit(1)
 		}
 		fmt.Printf("Loading configuration...\n")
 
 		cfg, err := configloader.Load(cp, factories)
 		if err != nil {
 			log.Fatalf("Cannot load configuration: %v", err)
+			os.Exit(1)
 		}
 
 		err = cfg.Validate()
 		if err != nil {
 			fmt.Printf("Invalid configuration: %w\n", err)
+			os.Exit(1)
 		}
 	} else {
 		log.Fatalf("Please provide a config file using the --config flag to validate\n")
