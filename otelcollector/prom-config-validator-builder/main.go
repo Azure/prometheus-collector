3e08767f11b5b72b0901fb0dc05a63efc8956724
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-09-16 21:19:45 -0700

    removing unwanted structs

--- otelcollector/prom-config-validator-builder/main.go
+++ otelcollector/prom-config-validator-builder/main.go
@@ -9,7 +9,6 @@ import (
 
 	"strings"
 
-	//"github.com/spf13/viper"
 	"go.opentelemetry.io/collector/config/configloader"
 	parserProvider "go.opentelemetry.io/collector/service/parserprovider"
 	yaml "gopkg.in/yaml.v2"
@@ -20,88 +19,14 @@ type OtelConfig struct {
 	Processors interface{} `yaml:"processors"`
 	Receivers  struct {
 		Prometheus struct {
-			//Config PrometheusConfig `yaml:"config"`
 			Config interface{} `yaml:"config"`
 		} `yaml:"prometheus"`
 	} `yaml:"receivers"`
 	Service interface{} `yaml:"service"`
 }
 
-type RelabelConfig struct {
-	SourceLabels interface{} `yaml:"source_labels,flow,omitempty"`
-	Separator    interface{} `yaml:"separator,omitempty"`
-	Regex        string      `yaml:"regex,omitempty"`
-	Modulus      interface{} `yaml:"modulus,omitempty"`
-	TargetLabel  interface{} `yaml:"target_label,omitempty"`
-	Replacement  string      `yaml:"replacement,omitempty"`
-	Action       interface{} `yaml:"action,omitempty"`
-}
-
-type ScrapeConfig struct {
-	JobName                 interface{}      `yaml:"job_name"`
-	HonorLabels             interface{}      `yaml:"honor_labels,omitempty"`
-	HonorTimestamps         interface{}      `yaml:"honor_timestamps"`
-	Params                  interface{}      `yaml:"params,omitempty"`
-	ScrapeInterval          interface{}      `yaml:"scrape_interval,omitempty"`
-	ScrapeTimeout           interface{}      `yaml:"scrape_timeout,omitempty"`
-	MetricsPath             interface{}      `yaml:"metrics_path,omitempty"`
-	Scheme                  interface{}      `yaml:"scheme,omitempty"`
-	BodySizeLimit           interface{}      `yaml:"body_size_limit,omitempty"`
-	SampleLimit             interface{}      `yaml:"sample_limit,omitempty"`
-	TargetLimit             interface{}      `yaml:"target_limit,omitempty"`
-	LabelLimit              interface{}      `yaml:"label_limit,omitempty"`
-	LabelNameLengthLimit    interface{}      `yaml:"label_name_length_limit,omitempty"`
-	LabelValueLengthLimit   interface{}      `yaml:"label_value_length_limit,omitempty"`
-	ServiceDiscoveryConfigs interface{}      `yaml:"-"`
-	HTTPClientConfig        struct{}         `yaml:",inline"`
-	RelabelConfigs          []*RelabelConfig `yaml:"relabel_configs,omitempty"`
-	MetricRelabelConfigs    []*RelabelConfig `yaml:"metric_relabel_configs,omitempty"`
-}
-
-type AlertingConfig struct {
-	AlertRelabelConfigs []interface{} `yaml:"alert_relabel_configs,omitempty"`
-	AlertmanagerConfigs []interface{} `yaml:"alertmanagers,omitempty"`
-}
-
-type PrometheusConfig struct {
-	GlobalConfig   interface{}     `yaml:"global"`
-	AlertingConfig AlertingConfig  `yaml:"alerting,omitempty"`
-	RuleFiles      []interface{}   `yaml:"rule_files,omitempty"`
-	ScrapeConfigs  []*ScrapeConfig `yaml:"scrape_configs,omitempty"`
-	StorageConfig  interface{}     `yaml:"storage,omitempty"`
-
-	RemoteWriteConfigs []interface{} `yaml:"remote_write,omitempty"`
-	RemoteReadConfigs  []interface{} `yaml:"remote_read,omitempty"`
-}
-
 func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTemplatePath string) error {
 	var otelConfig OtelConfig
-
-	//test code here
-
-	// viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
-
-	// any approach to require this configuration into your program.
-	// 	var yamlExample = []byte(`
-	// Hacker: true
-	// name: steve
-	// hobbies:
-	// - skateboarding
-	// - snowboarding
-	// - go
-	// clothing:
-	//   jacket: leather
-	//   trousers: denim
-	// age: 35
-	// eyes : brown
-	// beard: true
-	// `)
-
-	// viper.ReadConfig(bytes.NewBuffer(yamlExample))
-
-	// fmt.Printf("name: %v\n", viper.Get("name"))
-
-	// test code here
 
 	otelConfigFileContents, err := ioutil.ReadFile(otelConfigTemplatePath)
 	if err != nil {
@@ -112,19 +37,7 @@ func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTe
 		return err
 	}
 
-	var promConfig PrometheusConfig
-
 	promConfigFileContents, err := ioutil.ReadFile(promFilePath)
-	if err != nil {
-		return err
-	}
-	// viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
-
-	// viper.ReadConfig(bytes.NewBuffer(promConfigFileContents))
-
-	// fmt.Printf("scrape configs: %v\n", viper.Get("scrape_configs"))
-
-	err = yaml.Unmarshal([]byte(promConfigFileContents), &promConfig)
 	if err != nil {
 		return err
 	}
@@ -135,28 +48,21 @@ func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTe
 		return err
 	}
 
-	//fmt.Printf("%v\n", test["scrape_configs"])
 	var sc = prometheusConfig["scrape_configs"].([]interface{})
 	for _, scrapeConfig := range sc {
-		//fmt.Printf("rashmi -%v\n", rash)
-		//fmt.Printf("%v\n", rash)
 		scrapeConfig := scrapeConfig.(map[interface{}]interface{})
 		if scrapeConfig["relabel_configs"] != nil {
 			relabelConfigs := scrapeConfig["relabel_configs"].([]interface{})
-			//fmt.Printf("relabelConfig -%v\n", scrapeConfig["relabel_configs"])
 			for _, relabelConfig := range relabelConfigs {
-				// fmt.Printf("%v\n", relabelConfig)
 				relabelConfig := relabelConfig.(map[interface{}]interface{})
-				//fmt.Printf("%v\n", relabelConfig["regex"])
-				//replace $ with $$ for regex
+				//replace $ with $$ for regex field
 				if relabelConfig["regex"] != nil {
 					regexString := relabelConfig["regex"].(string)
 					modifiedRegexString := strings.ReplaceAll(regexString, "$$", "$")
 					modifiedRegexString = strings.ReplaceAll(modifiedRegexString, "$", "$$")
 					relabelConfig["regex"] = modifiedRegexString
-					//fmt.Printf("%v\n", relabelConfig)
 				}
-				//replace $ with $$ for replacement
+				//replace $ with $$ for replacement field
 				if relabelConfig["replacement"] != nil {
 					replacement := relabelConfig["replacement"].(string)
 					modifiedReplacementString := strings.ReplaceAll(replacement, "$$", "$")
@@ -166,27 +72,22 @@ func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTe
 					}
 					relabelConfig["replacement"] = modifiedReplacementString
 				}
-				fmt.Printf("relabelConfig - %v\n", relabelConfig)
 			}
 		}
 
 		if scrapeConfig["metric_relabel_configs"] != nil {
 			metricRelabelConfigs := scrapeConfig["metric_relabel_configs"].([]interface{})
-			//fmt.Printf("relabelConfig -%v\n", scrapeConfig["relabel_configs"])
 			for _, metricRelabelConfig := range metricRelabelConfigs {
-				// fmt.Printf("%v\n", relabelConfig)
 				metricRelabelConfig := metricRelabelConfig.(map[interface{}]interface{})
-				//fmt.Printf("%v\n", relabelConfig["regex"])
-				//replace $ with $$ for regex
+				//replace $ with $$ for regex field
 				if metricRelabelConfig["regex"] != nil {
 					regexString := metricRelabelConfig["regex"].(string)
 					modifiedRegexString := strings.ReplaceAll(regexString, "$$", "$")
 					modifiedRegexString = strings.ReplaceAll(modifiedRegexString, "$", "$$")
 					metricRelabelConfig["regex"] = modifiedRegexString
 				}
-				//fmt.Printf("%v\n", relabelConfig)
 
-				//replace $ with $$ for replacement
+				//replace $ with $$ for replacement field
 				if metricRelabelConfig["replacement"] != nil {
 					replacement := metricRelabelConfig["replacement"].(string)
 					modifiedReplacementString := strings.ReplaceAll(replacement, "$$", "$")
@@ -196,8 +97,6 @@ func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTe
 					}
 					metricRelabelConfig["replacement"] = modifiedReplacementString
 				}
-				fmt.Printf("metricRelabelConfig - %v\n", metricRelabelConfig)
-
 			}
 		}
 	}
@@ -205,64 +104,30 @@ func generateOtelConfig(promFilePath string, outputFilePath string, otelConfigTe
 	// Need this here even though it is present in the receiver's config validate method since we only do the $ manipulation for regex and replacement fields
 	// in scrape configs sections and the load method which is called before the validate method fails to unmarshal due to single $.
 	// Either approach will fail but the receiver's config load wont return the right error message
-	unsupportedFeatures := make([]string, 0, 4)
-	if len(promConfig.RemoteWriteConfigs) != 0 {
-		unsupportedFeatures = append(unsupportedFeatures, "remote_write")
-	}
-	if len(promConfig.RemoteReadConfigs) != 0 {
-		unsupportedFeatures = append(unsupportedFeatures, "remote_read")
-	}
-	if len(promConfig.RuleFiles) != 0 {
-		unsupportedFeatures = append(unsupportedFeatures, "rule_files")
-	}
-	if len(promConfig.AlertingConfig.AlertRelabelConfigs) != 0 {
-		unsupportedFeatures = append(unsupportedFeatures, "alert_config.relabel_configs")
-	}
-	if len(promConfig.AlertingConfig.AlertmanagerConfigs) != 0 {
-		unsupportedFeatures = append(unsupportedFeatures, "alert_config.alertmanagers")
-	}
-	if len(unsupportedFeatures) != 0 {
-		return fmt.Errorf("unsupported features:\n\t%s", strings.Join(unsupportedFeatures, "\n\t"))
-	}
+	// unsupportedFeatures := make([]string, 0, 4)
+	// if len(promConfig.RemoteWriteConfigs) != 0 {
+	// 	unsupportedFeatures = append(unsupportedFeatures, "remote_write")
+	// }
+	// if len(promConfig.RemoteReadConfigs) != 0 {
+	// 	unsupportedFeatures = append(unsupportedFeatures, "remote_read")
+	// }
+	// if len(promConfig.RuleFiles) != 0 {
+	// 	unsupportedFeatures = append(unsupportedFeatures, "rule_files")
+	// }
+	// if len(promConfig.AlertingConfig.AlertRelabelConfigs) != 0 {
+	// 	unsupportedFeatures = append(unsupportedFeatures, "alert_config.relabel_configs")
+	// }
+	// if len(promConfig.AlertingConfig.AlertmanagerConfigs) != 0 {
+	// 	unsupportedFeatures = append(unsupportedFeatures, "alert_config.alertmanagers")
+	// }
+	// if len(unsupportedFeatures) != 0 {
+	// 	return fmt.Errorf("unsupported features:\n\t%s", strings.Join(unsupportedFeatures, "\n\t"))
+	// }
 
-	if err != nil {
-		return err
-	}
-	for _, scfg := range promConfig.ScrapeConfigs {
-		for _, relabelConfig := range scfg.RelabelConfigs {
-			regexString := relabelConfig.Regex
-			// Replacing $$ with $ for backward compatibility, since golang doesnt support lookarounds cannot use this regex /(?<!\$)\$(?!\$)/ for checking single $
-			modifiedRegexString := strings.ReplaceAll(regexString, "$$", "$")
-			modifiedRegexString = strings.ReplaceAll(modifiedRegexString, "$", "$$")
-			relabelConfig.Regex = modifiedRegexString
+	// if err != nil {
+	// 	return err
+	// }
 
-			replacement := relabelConfig.Replacement
-			modifiedReplacementString := strings.ReplaceAll(replacement, "$$", "$")
-			modifiedReplacementString = strings.ReplaceAll(modifiedReplacementString, "$", "$$")
-			if err != nil {
-				return err
-			}
-			relabelConfig.Replacement = modifiedReplacementString
-		}
-		for _, metricRelabelConfig := range scfg.MetricRelabelConfigs {
-			regexString := metricRelabelConfig.Regex
-			// Replacing $$ with $ for backward compatibility, since golang doesnt support lookarounds cannot use this regex /(?<!\$)\$(?!\$)/ for checking single $
-			modifiedRegexString := strings.ReplaceAll(regexString, "$$", "$")
-			modifiedRegexString = strings.ReplaceAll(modifiedRegexString, "$", "$$")
-
-			metricRelabelConfig.Regex = modifiedRegexString
-
-			replacement := metricRelabelConfig.Replacement
-			modifiedReplacementString := strings.ReplaceAll(replacement, "$$", "$")
-			modifiedReplacementString = strings.ReplaceAll(modifiedReplacementString, "$", "$$")
-			if err != nil {
-				return err
-			}
-			metricRelabelConfig.Replacement = modifiedReplacementString
-		}
-	}
-
-	//otelConfig.Receivers.Prometheus.Config = promConfig
 	otelConfig.Receivers.Prometheus.Config = prometheusConfig
 
 	mergedConfig, err := yaml.Marshal(otelConfig)
