440f10c363fc5bfbc8aa15a9650471203a85fb8c
vishwanath <visnara@microsoft.com>
2021-10-12 13:59:38 -0700

    fix docs & release script

--- otelcollector/README.md
+++ otelcollector/README.md
@@ -28,10 +28,11 @@ Each merge into `main` will push the image to the public mcr and deploy to the d
 - **PR 1**: Bump the version in the VERSION file following semantic versioning.
     - If you know your PR with the last feature changes will be the last one before the release, you can do this then.
     - **Build 1**: The `values.yaml` and `Chart.yaml` templates for the HELM chart will automatically be replaced with the image tag and the HELM chart version during the workflow build.
-- **PR 2**: Get the commit SHA from the commit used for **Build 1** and run `./release.sh` script: 
+- **PR 2**: Get the chart semver or container image tag from the commit used for **Build 1**, and also for the current reeased one and run `./release.sh` script: 
 
   - ```
-    commit_id=<commit id> ./release.sh
+    previous_semver=<semver_for_currently_relased_chart> current_semver=<semver_for_to_be_relased_chart_from_pr_1_above> ./release.sh
+    ex;- previous_semver=0.0.5-main-10-11-2021-4a9de500 current_semver=0.0.6-main-10-12-2021-5c34d764 ./release.sh
     ```
   - This changes the image and helm chart tags in all the README files that contain it.
 - **PR 3**: Make a PR to update the [Geneva docs](https://msazure.visualstudio.com/One/_git/EngSys-MDA-GenevaDocs?path=%2Fdocumentation%2Fmetrics%2FPrometheus&version=GBmaster&_a=contents) with any changes made in `/otelcollector/deploy/eng.ms/docs/Prometheus`
--- otelcollector/release.sh
+++ otelcollector/release.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
-if [ -z "$commit_id" ]
+if [ -z "$previous_semver" ]
 then
-  echo "commit_id is not set. Re-run as: commit_id=<number> ./release.sh"
+  echo "previous_semver is not set. Re-run as: previous_semver=prevsemver current_semver=currsemver ./release.sh"
   exit
 fi
 
-if [ ${#commit_id} -lt 8 ]
+if [ -z "$current_semver" ]
 then
-  echo "commit_id is less than 8 characters. Re-run with longer commit id."
+  echo "current_semver is not set. Re-run as: previous_semver=prevsemver current_semver=currsemver ./release.sh"
   exit
 fi
 
-if [ ${#commit_id} -gt 8 ]
-then
-  commit_id=$(echo $commit_id | cut -c 1-8)
-  echo "truncating commit_id to: ${commit_id}"
-fi
+semver_regex="[0-9.]-main-\+"
+echo "Replacing $previous_semver globally with $current_semver"
+for file in $(find ../ -name "*.md" -o -name "*.yaml" -o -name "*.yml" -type f); do echo -e "\n$file: "; sed -i "s/$previous_semver/$current_semver/g" $file; done
 
-version=$(cat ./VERSION)
-date=$(TZ=America/Los_Angeles date +%m-%d-%Y)
-#0.0.5-main-10-11-2021-4a8de406 prometheus-collector-chart-main-0.0.5-09-25-2021-e1c22c83
-image_regex="prometheus-collector-main-[a-zA-Z0-9.-]\+"
-image_replacement="prometheus-collector-main-$version-$date-$commit_id"
-echo "Replacing the image tag with:      $image_replacement"
-sed -i "s/$image_regex/$image_replacement/g" ./deploy/prometheus-collector.yaml
-sed -i "s/$image_regex/$image_replacement/g" ./deploy/chart/prometheus-collector/README.md
-sed -i "s/$image_regex/$image_replacement/g" ./deploy/eng.ms/docs/Prometheus/chartvalues.md
-
-helm_regex="prometheus-collector-chart-main-[a-zA-Z0-9.-]\+"
-helm_replacement="prometheus-collector-chart-main-$version-$date-$commit_id"
-echo "Replacing the HELM chart tag with: $helm_replacement"
-sed -i "s/$helm_regex/$helm_replacement/g" ./deploy/eng.ms/docs/Prometheus/PromMDMTutorial2DeployAgentHELM.md
-sed -i "s/$helm_regex/$helm_replacement/g" ./deploy/chart/prometheus-collector/README.md
