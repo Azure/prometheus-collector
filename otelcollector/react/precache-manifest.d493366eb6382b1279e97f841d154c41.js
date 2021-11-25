4bee27b4057dcbd7e875b5790d983fff72d9066a
Grace Wehner <grace.wehner@microsoft.com>
2021-11-24 16:08:29 -0800

    latest prom ui, fix binskim

---
+++ otelcollector/react/precache-manifest.d493366eb6382b1279e97f841d154c41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c6505023db1c4ef8ec1136d95244e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423cff5c243efaa6f92c",
+    "url": "/static/css/2.7d03da45.chunk.css"
+  },
+  {
+    "revision": "35c680d7ff69f32fe01b",
+    "url": "/static/css/main.ca5f99a5.chunk.css"
+  },
+  {
+    "revision": "423cff5c243efaa6f92c",
+    "url": "/static/js/2.202d2d88.chunk.js"
+  },
+  {
+    "revision": "d809ecb49c9faa7cafeb827ad9f4c773",
+    "url": "/static/js/2.202d2d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c680d7ff69f32fe01b",
+    "url": "/static/js/main.22c39560.chunk.js"
+  },
+  {
+    "revision": "eeb81b01ebf9e4ecbb58",
+    "url": "/static/js/runtime-main.bd49e38d.js"
+  },
+  {
+    "revision": "223490291528837216424bf892a36810",
+    "url": "/static/media/codicon.22349029.ttf"
+  }
+]);
