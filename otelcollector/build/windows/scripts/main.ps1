96e02fe00e8145a03d63abd056009229cf2b011a
vishwanath <visnara@microsoft.com>
2022-02-11 13:34:42 -0800

    fix int deployment for windows

--- otelcollector/build/windows/scripts/main.ps1
+++ otelcollector/build/windows/scripts/main.ps1
@@ -265,6 +265,7 @@ function Start-ME {
     Start-Job -ScriptBlock { 
         $me_config_file = $env:ME_CONFIG_FILE
         $AZMON_DEFAULT_METRIC_ACCOUNT_NAME = $env:AZMON_DEFAULT_METRIC_ACCOUNT_NAME
+        $ME_ADDITIONAL_FLAGS = $env:ME_ADDITIONAL_FLAGS
         Start-Process -NoNewWindow -FilePath "/opt/metricextension/MetricsExtension/MetricsExtension.Native.exe" -ArgumentList @("-Logger", "File", "-LogLevel", "Info", "-DataDirectory", ".\", "-Input", "otlp_grpc", "-MonitoringAccount", $AZMON_DEFAULT_METRIC_ACCOUNT_NAME, "-ConfigOverridesFilePath", $me_config_file, $ME_ADDITIONAL_FLAGS) 
     }
     tasklist /fi "imagename eq MetricsExtension.Native.exe" /fo "table"  | findstr MetricsExtension
