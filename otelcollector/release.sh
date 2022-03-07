df71b65a0c0ec7f55ffb32d2e5eabf0d73fd34ad
Vishwanath <visnara@microsoft.com>
2022-03-07 00:22:06 -0800

    Update release.sh

--- otelcollector/release.sh
+++ otelcollector/release.sh
@@ -15,6 +15,6 @@ fi
 semver_regex="[0-9.]-main-\+"
 echo "Replacing $previous_semver globally with $current_semver in docs folder"
 for file in $(find ./docs/ ! -name "PromMDMReleaseNotes.md" -name "*.md" -o -name "*.yaml" -o -name "*.yml" -type f); do echo -e "\n$file: "; sed -i "s/$previous_semver/$current_semver/g" $file; done
-# Replacing the image in scan-released-image.yml
+# Replacing the image  in scan-released-image.yml
 echo "Replacing $previous_semver globally with $current_semver in github workflow - scan-released-image.yml"
 sed -i "s/$previous_semver/$current_semver/g" ../.github/workflows/scan-released-image.yml
