3e4aee73b684376fd491c2d60774dead8c6979ac
Grace Wehner <grace.wehner@microsoft.com>
2022-03-07 17:16:09 -0800

    0308 ME drop

--- otelcollector/build/windows/scripts/setup.ps1
+++ otelcollector/build/windows/scripts/setup.ps1
@@ -11,8 +11,8 @@ New-Item -Type Directory -Path /opt/ruby -ErrorAction SilentlyContinue
 ###########################################################################################
 Write-Host ('Installing Metrics Extension');
 try {
-    Invoke-WebRequest -Uri https://github.com/microsoft/Docker-Provider/releases/download/ME-OTEL-WINDOWS-TEST/MdmMetricsExtension.2.2022.305.243.nupkg -OutFile /installation/ME/mdmmetricsextension.2.2022.305.243.zip
-    Expand-Archive -Path /installation/ME/mdmmetricsextension.2.2022.303.119.zip -Destination /installation/ME/
+    Invoke-WebRequest -Uri https://github.com/microsoft/Docker-Provider/releases/download/ME-OTEL-WINDOWS-TEST/MdmMetricsExtension.2.2022.308.30.nupkg -OutFile /installation/ME/mdmmetricsextension.2.2022.308.30.zip
+    Expand-Archive -Path /installation/ME/mdmmetricsextension.2.2022.308.30.zip -Destination /installation/ME/
     Move-Item /installation/ME/MetricsExtension /opt/metricextension/
 }
 catch {
