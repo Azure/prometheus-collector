0dd4165df804cd8b37c6d9c419144d7b52fe2109
Grace Wehner <grace.wehner@microsoft.com>
2022-06-09 21:05:43 -0700

    new ME

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -83,7 +83,7 @@ cp /etc/cron.daily/logrotate /etc/cron.hourly/
 
 # Moving ME installation to the end until we fix the broken dependencies issue
 sudo apt-get install -y apt-transport-https gnupg
-wget https://github.com/microsoft/Docker-Provider/releases/download/04012021/metricsext2_2.2022.531.118-53e971-_focal_amd64.deb
+wget https://github.com/microsoft/Docker-Provider/releases/download/04012021/metricsext2_2.2022.609.1207-6cbe96-_focal_amd64.deb
 
 # # Install ME
 /usr/bin/dpkg -i $TMPDIR/metricsext2*.deb
