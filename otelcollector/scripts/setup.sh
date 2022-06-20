ce8fb268d9aff978103a88dce9f7d691e8b316d0
vishwanath <visnara@microsoft.com>
2022-06-19 18:02:49 -0700

    try with newer updates

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -83,7 +83,7 @@ cp /etc/cron.daily/logrotate /etc/cron.hourly/
 
 # Moving ME installation to the end until we fix the broken dependencies issue
 sudo apt-get install -y apt-transport-https gnupg
-wget https://github.com/Azure/prometheus-collector/tree/vishwa/latency06192022/otelcollector/metricextension/metricsext2_2.2022.610.015-3c1809-_focal_amd64.deb
+wget https://github.com/microsoft/Docker-Provider/blob/vishwa/latency/metricsext2_2.2022.610.015-3c1809-_focal_amd64.deb
 #wget https://github.com/microsoft/Docker-Provider/releases/download/04012021/metricsext2_2.2022.609.1207-6cbe96-_focal_amd64.deb
 
 # # Install ME
