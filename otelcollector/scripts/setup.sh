7e335a76d8c9183c7a3d4c67403435892cf7ad6e
Grace Wehner <grace.wehner@microsoft.com>
2022-07-15 16:54:50 -0700

    use tdnf but remove the packages that were autoremoved

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -14,12 +14,12 @@ chmod 744 /usr/sbin/
 
 #download inotify tools for watching configmap changes
 echo "Installing inotify..."
-sudo dnf check-update
-sudo dnf repolist --refresh
-sudo dnf install inotify-tools -y
+sudo tdnf check-update
+sudo tdnf repolist --refresh
+sudo tdnf install inotify-tools -y
 
 echo "Installing packages for re2 gem install..."
-sudo dnf install -y build-essential re2-devel
+sudo tdnf install -y build-essential re2-devel
 
 echo "Installing tomlrb, deep_merge and re2 gems..."
 gem install colorize
@@ -53,20 +53,20 @@ gem install re2
 #chmod 777 /opt/microsoft/otelcollector29/otelcollector
 
 echo "Installing MDSD dependencies"
-sudo dnf install -y which
+sudo tdnf install -y which
 echo "Downloading MDSD"
-sudo dnf --disablerepo="*" --enablerepo=packages-microsoft-com-azurecore install azure-mdsd-1.18.0 -y
+sudo tdnf --disablerepo="*" --enablerepo=packages-microsoft-com-azurecore install azure-mdsd-1.18.0 -y
 cp -f $TMPDIR/envmdsd /etc/mdsd.d
 # Create the following directory for logs
 mkdir /opt/microsoft/linuxmonagent
 
 # Install Telegraf
 echo "Installing telegraf..."
-sudo dnf install telegraf-1.23.0 -y
+sudo tdnf install telegraf-1.23.0 -y
 
 # Install fluent-bit
 echo "Installing fluent-bit..."
-sudo dnf install fluent-bit-1.8.12 -y
+sudo tdnf install fluent-bit-1.8.12 -y
 
 # setup hourly cron for logrotate
 cp /etc/cron.daily/logrotate /etc/cron.hourly/
@@ -104,10 +104,10 @@ echo "Installing Metrics Extension..."
 #/usr/bin/dpkg -i $TMPDIR/metricsext2*.deb
 
 wget https://github.com/microsoft/Docker-Provider/releases/download/04012021/metricsext2-2.2022.628.2309-1.cm2.x86_64.rpm
-sudo dnf install -y metricsext2-2.2022.628.2309-1.cm2.x86_64.rpm
+sudo tdnf install -y metricsext2-2.2022.628.2309-1.cm2.x86_64.rpm
 
 # tdnf does not have an autoremove feature. Only necessary packages are copied over to distroless build.
-sudo tdnf remove g++ binutils libgcc-atomic make patch -y
-sudo dnf autoremove -y
+sudo tdnf remove g++ binutils libgcc-atomic make patch bison diffutils docbook-dtd-xml gawk glibc-devel installkernel kernel-headers libgcc-devel libgomp-devel libmpc libstdc++-devel libtool libxml2-devel libxslt m4 mariner-rpm-macros mpfr python3-lxml python3-pygments dnf -y
+#sudo dnf autoremove -y
 
 rm -f $TMPDIR/metricsext2*.rpm
