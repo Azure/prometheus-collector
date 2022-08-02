6d119fa977567e3f5d0eb087ac4959b588bd3398
Grace Wehner <grace.wehner@microsoft.com>
2022-08-01 17:25:25 -0700

    ev2 updates

--- .pipelines/deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
+++ .pipelines/deployment/ServiceGroupRoot/Scripts/pushAgentToAcr.sh
@@ -3,61 +3,49 @@ set -e
 
 # Note - This script used in the pipeline as inline script
 
-if [ -z $AGENT_IMAGE_TAG_SUFFIX ]; then
-  echo "-e error value of AGENT_IMAGE_TAG_SUFFIX variable shouldnt be empty. check release variables"
+if [ -z $IMAGE_TAG ]; then
+  echo "-e error value of IMAGE_TAG variable shouldnt be empty. Check release variables"
   exit 1
 fi
 
-if [ -z $AGENT_RELEASE ]; then
-  echo "-e error AGENT_RELEASE shouldnt be empty. check release variables"
+if [ -z $MCR_REGISTRY ]; then
+  echo "-e error MCR_REGISTRY shouldnt be empty. Check release variables"
+  exit 1
+fi
+
+if [ -z $PROD_MCR_REPOSITORY ]; then
+  echo "-e error PROD_MCR_REPOSITORY shouldnt be empty. Check release variables"
+  exit 1
+fi
+
+if [ -z $DEV_MCR_REPOSITORY ]; then
+  echo "-e error value of DEV_MCR_REPOSITORY shouldn't be empty. Check release variables"
+  exit 1
+fi
+
+if [ -z $ACR_REGISTRY ]; then
+  echo "-e error value of ACR_REGISTRY shouldn't be empty. Check release variables"
+  exit 1
+fi
+
+if [ -z $PROD_ACR_REPOSITORY ]; then
+  echo "-e error value of PROD_ACR_REPOSITORY shouldn't be empty. Check release variables"
   exit 1
 fi
 
 #Make sure that tag being pushed will not overwrite an existing tag in mcr
-MCR_TAG_RESULT="`wget -qO- https://mcr.microsoft.com/v2/azuremonitor/containerinsights/ciprod/tags/list`"
+PROD_MCR_TAG_RESULT="`wget -qO- https://mcr.microsoft.com/v2$PROD_MCR_REPOSITORY/tags/list`"
 if [ $? -ne 0 ]; then         
-   echo "-e error unable to get list of mcr tags for azuremonitor/containerinsights/ciprod repository"
+   echo "-e error unable to get list of tags for $PROD_MCR_REPOSITORY"
    exit 1
 fi
 
-if [[ "$AGENT_IMAGE_FULL_PATH" == *"win-"* ]]; then
-  echo "checking windows tags"
-  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"win-$AGENT_RELEASE$AGENT_IMAGE_TAG_SUFFIX"'"])')
-else
-  echo "checking linux tags"
-  TAG_EXISTS=$(echo $MCR_TAG_RESULT | jq '.tags | contains(["'"$AGENT_RELEASE$AGENT_IMAGE_TAG_SUFFIX"'"])')
-fi
+TAG_EXISTS=$(echo $PROD_MCR_TAG_RESULT | jq '.tags | contains(["'"$IMAGE_TAG"'"])')
 
 if $TAG_EXISTS; then
-  echo "-e error ${AGENT_IMAGE_TAG_SUFFIX} already exists in mcr. make sure the image tag is unique"
+  echo "-e error ${IMAGE_TAG} already exists in Prod MCR. Make sure the image tag is unique"
   exit 1
 fi
-
-if [ -z $AGENT_IMAGE_FULL_PATH ]; then
-  echo "-e error AGENT_IMAGE_FULL_PATH shouldnt be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_TAG ]; then
-  echo "-e error value of CDPX_TAG shouldn't be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_REGISTRY ]; then
-  echo "-e error value of CDPX_REGISTRY shouldn't be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $CDPX_REPO_NAME ]; then
-  echo "-e error value of CDPX_REPO_NAME shouldn't be empty. check release variables"
-  exit 1
-fi
-
-if [ -z $ACR_NAME ]; then
-  echo "-e error value of ACR_NAME shouldn't be empty. check release variables"
-  exit 1
-fi
-
 
 #Login to az cli and authenticate to acr
 echo "Login cli using managed identity"
@@ -67,10 +55,10 @@ if [ $? -eq 0 ]; then
 else
   echo "-e error failed to login to az with managed identity credentials"
   exit 1
-fi     
+fi
 
-echo "Pushing ${AGENT_IMAGE_FULL_PATH} to ${ACR_NAME}"
-az acr import --name $ACR_NAME --registry $CDPX_REGISTRY --source official/${CDPX_REPO_NAME}:${CDPX_TAG} --image $AGENT_IMAGE_FULL_PATH
+echo "Pushing $PROD_ACR_REPOSITORY:$IMAGE_TAG to ${ACR_NAME}"
+az acr import --name $ACR_REGISTRY --source ${MCR_REGISTRY}${MCR_REPOSITORY}:${IMAGE_TAG} --image ${PROD_ACR_REPOSITORY}:${IMAGE_TAG}
 if [ $? -eq 0 ]; then
   echo "Retagged and pushed image successfully"
 else
