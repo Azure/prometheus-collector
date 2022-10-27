e37f6b725862810bd0e0a237946374a74c698d4a
Grace Wehner <grace.wehner@microsoft.com>
2022-10-27 00:32:23 -0700

    cleanup

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -26,10 +26,14 @@ sudo tdnf repolist --refresh
 sudo tdnf install inotify-tools -y
 
 echo "Installing packages for re2 gem install..."
-sudo tdnf install -y build-essential re2-devel rpmdevtools
+sudo tdnf install -y build-essential re2-devel
 
-echo "Installing MDSD dependencies"
-sudo tdnf install -y which
+echo "Installing tomlrb, deep_merge and re2 gems..."
+gem install colorize
+gem install tomlrb
+gem install deep_merge
+gem install re2
+
 echo "Downloading MDSD"
 if [ "${ARCH}" != "amd64" ]; then
   wget https://github.com/microsoft/Docker-Provider/releases/download/mdsd-mac-official-06-13/azure-mdsd_1.19.3-build.master.428_aarch64.rpm
@@ -45,15 +49,13 @@ mkdir /opt/microsoft/linuxmonagent
 
 # Install Telegraf
 echo "Installing telegraf..."
-# TODO: update
 sudo tdnf install telegraf-1.23.0 -y
 
-# TODO: goes from 1.9.7 to 1.9.6
 # Install fluent-bit
 echo "Installing fluent-bit..."
 sudo tdnf install fluent-bit-1.9.6 -y
 
-# setup hourly cron for logrotate
+# Setup hourly cron for logrotate
 cp /etc/cron.daily/logrotate /etc/cron.hourly/
 
 # Installing ME
@@ -63,16 +65,8 @@ if [ "${ARCH}" != "amd64" ]; then
   sudo tdnf install -y metricsext2-2.2022.1013.1515-1.cm2.aarch64.rpm
 else 
   sudo tdnf install -y metricsext2-2.2022.811.1333
-  sudo tdnf list installed | grep metricsext2 | awk '{print $2}' > metricsextversion.txt
 fi
-
-echo "Installing tomlrb, deep_merge and re2 gems..."
-gem install colorize
-gem install tomlrb
-gem install deep_merge
-gem install re2
-
-cat /usr/lib/ruby/gems/3.1.0/extensions/aarch64-linux/3.1.0/re2-1.5.0/mkmf.log
+sudo tdnf list installed | grep metricsext2 | awk '{print $2}' > metricsextversion.txt
 
 # tdnf does not have an autoremove feature. Only necessary packages are copied over to distroless build. Below reduces the image size if using non-distroless
 #sudo tdnf remove g++ binutils libgcc-atomic make patch bison diffutils docbook-dtd-xml gawk glibc-devel installkernel kernel-headers libgcc-devel libgomp-devel libmpc libstdc++-devel libtool libxml2-devel libxslt m4 mariner-rpm-macros mpfr python3-lxml python3-pygments dnf -y
