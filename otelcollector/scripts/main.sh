95949754e6062ef479d9ade08bf2a60eb38c5c89
Grace Wehner <grace.wehner@microsoft.com>
2023-02-14 18:18:10 -0800

    more cleaup

--- otelcollector/scripts/main.sh
+++ otelcollector/scripts/main.sh
@@ -22,8 +22,6 @@ echo_var () {
   echo -e "${Cyan}$1${Color_Off}=$2"
 }
 
-env
-
 #Run inotify as a daemon to track changes to the mounted configmap.
 touch /opt/inotifyoutput.txt
 inotifywait /etc/config/settings --daemon --recursive --outfile "/opt/inotifyoutput.txt" --event create,delete --format '%e : %T' --timefmt '+%s'
