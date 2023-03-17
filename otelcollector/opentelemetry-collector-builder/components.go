b6931f29300cccb98ccb57e14a8033adb98bfab7
Soham Dasgupta <sohdasgupta@microsoft.com>
2023-03-17 13:35:11 -0700

    test

--- otelcollector/opentelemetry-collector-builder/components.go
+++ otelcollector/opentelemetry-collector-builder/components.go
@@ -1,56 +1,57 @@
 package main
 
 import (
-	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/processor/batchprocessor"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourceprocessor"
 	"go.opentelemetry.io/collector/exporter/loggingexporter"
 	"go.opentelemetry.io/collector/exporter/otlpexporter"
+	"go.opentelemetry.io/collector/processor/batchprocessor"
+	"go.opentelemetry.io/collector/service"
+
 	//"github.com/vishiy/influxexporter"
 	//"go.opentelemetry.io/collector/receiver/prometheusreceiver"
 	privatepromreceiver "github.com/gracewehner/prometheusreceiver"
 	//"go.opentelemetry.io/collector/extension/healthcheckextension"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/pprofextension"
-	"go.opentelemetry.io/collector/extension/zpagesextension"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/fileexporter"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusexporter"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/extension/pprofextension"
+	"go.opentelemetry.io/collector/extension/zpagesextension"
 )
 
-func components() (component.Factories, error) {
-  var err error
-	factories := component.Factories{}
+func components() (service.Factories, error) {
+	var err error
+	factories := service.Factories{}
 
-	factories.Processors, err = component.MakeProcessorFactoryMap(
+	factories.Processors, err = service.MakeProcessorFactoryMap(
 		batchprocessor.NewFactory(),
 		resourceprocessor.NewFactory(),
 	)
 	if err != nil {
-		return component.Factories{}, err
+		return service.Factories{}, err
 	}
 
-	factories.Receivers, err = component.MakeReceiverFactoryMap(
+	factories.Receivers, err = service.MakeReceiverFactoryMap(
 		privatepromreceiver.NewFactory(),
 	)
 	if err != nil {
-		return component.Factories{}, err
+		return service.Factories{}, err
 	}
 
-	factories.Exporters, err = component.MakeExporterFactoryMap(
+	factories.Exporters, err = service.MakeExporterFactoryMap(
 		loggingexporter.NewFactory(),
 		otlpexporter.NewFactory(),
 		fileexporter.NewFactory(),
 		prometheusexporter.NewFactory(),
 	)
 	if err != nil {
-		return component.Factories{}, err
+		return service.Factories{}, err
 	}
 
-	factories.Extensions, err = component.MakeExtensionFactoryMap(
+	factories.Extensions, err = service.MakeExtensionFactoryMap(
 		pprofextension.NewFactory(),
 		zpagesextension.NewFactory(),
 	)
 	if err != nil {
-		return component.Factories{}, err
+		return service.Factories{}, err
 	}
 
 	return factories, nil
