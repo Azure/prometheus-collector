0f38bad0cece4bec8a3f07e5147b5078baacb700
Grace Wehner <grace.wehner@microsoft.com>
2023-05-05 10:33:46 -0700

    cleanup and fixes for Arc release

--- otelcollector/scripts/main.sh
+++ otelcollector/scripts/main.sh
@@ -26,6 +26,18 @@ echo_var () {
 touch /opt/inotifyoutput.txt
 inotifywait /etc/config/settings --daemon --recursive --outfile "/opt/inotifyoutput.txt" --event create,delete --format '%e : %T' --timefmt '+%s'
 
+export IS_ARC_CLUSTER="false"
+CLUSTER_nocase=$(echo $CLUSTER | tr "[:upper:]" "[:lower:]")
+if [[ $CLUSTER_nocase =~ "connectedclusters" ]]; then
+  export IS_ARC_CLUSTER="true"
+fi
+echo "export IS_ARC_CLUSTER=$IS_ARC_CLUSTER" >> ~/.bashrc
+
+# EULA statement is required for Arc extension
+if [ "$IS_ARC_CLUSTER" == "true" ]; then
+  echo "MICROSOFT SOFTWARE LICENSE TERMS\n\nMICROSOFT Azure Arc-enabled Kubernetes\n\nThis software is licensed to you as part of your or your company's subscription license for Microsoft Azure Services. You may only use the software with Microsoft Azure Services and subject to the terms and conditions of the agreement under which you obtained Microsoft Azure Services. If you do not have an active subscription license for Microsoft Azure Services, you may not use the software. Microsoft Azure Legal Information: https://azure.microsoft.com/en-us/support/legal/"
+fi
+
 if [ -z $MODE ]; then
   MODE="simple"
 fi
@@ -61,18 +73,6 @@ if [ "$HTTP_PROXY" != "" ]; then
   export HTTP_PROXY_ENABLED="true"
 fi
 echo "export HTTP_PROXY_ENABLED=$HTTP_PROXY_ENABLED" >> ~/.bashrc
-
-export IS_ARC_CLUSTER="false"
-CLUSTER_nocase=$(echo $CLUSTER | tr "[:upper:]" "[:lower:]")
-if [[ $CLUSTER_nocase =~ "connectedclusters" ]]; then
-  export IS_ARC_CLUSTER="true"
-fi
-echo "export IS_ARC_CLUSTER=$IS_ARC_CLUSTER" >> ~/.bashrc
-
-# EULA statement is required for Arc extension
-if IS_ARC_CLUSTER; then
-  echo "MICROSOFT SOFTWARE LICENSE TERMS\n\nMICROSOFT Azure Arc-enabled Kubernetes\n\nThis software is licensed to you as part of your or your company's subscription license for Microsoft Azure Services. You may only use the software with Microsoft Azure Services and subject to the terms and conditions of the agreement under which you obtained Microsoft Azure Services. If you do not have an active subscription license for Microsoft Azure Services, you may not use the software. Microsoft Azure Legal Information: https://azure.microsoft.com/en-us/support/legal/"
-fi
 
 if [ $IS_ARC_CLUSTER == "true" ] && [ $HTTP_PROXY_ENABLED == "true" ]; then
   proxyprotocol="$(echo $HTTPS_PROXY | grep :// | sed -e's,^\(.*://\).*,\1,g')"
