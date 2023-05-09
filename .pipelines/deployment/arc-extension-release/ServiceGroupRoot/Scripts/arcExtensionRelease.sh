3de5cb472a93e68b52561934280b63b4ed70f775
Grace Wehner <grace.wehner@microsoft.com>
2023-05-09 10:55:38 -0700

    more script fixes

--- .pipelines/deployment/arc-extension-release/ServiceGroupRoot/Scripts/arcExtensionRelease.sh
+++ .pipelines/deployment/arc-extension-release/ServiceGroupRoot/Scripts/arcExtensionRelease.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 export HELM_EXPERIMENTAL_OCI=1
 
-IS_CUSTOMER_HIDDEN=${IS_CUSTOMER_HIDDEN:-true}
-CHART_VERSION=${CHART_VERSION}
-RELEASE_STAGE=${RELEASE_STAGE:-pipeline}
-
+IS_CUSTOMER_HIDDEN="${IS_CUSTOMER_HIDDEN:-true}"
+CHART_VERSION="${CHART_VERSION}"
 PACKAGE_CONFIG_NAME="${PACKAGE_CONFIG_NAME:-Microsoft.AzureMonitor.Containers.Metrics-Prom041823}"
 API_VERSION="${API_VERSION:-2021-05-01}"
 METHOD="${METHOD:-put}"
-REGISTRY_PATH="https://mcr.microsoft.com/azuremonitor/containerinsights/ciprod/ama-metrics-arc"
+REGISTRY_PATH="${REGISTRY_PATH:-mcr.microsoft.com/azuremonitor/containerinsights/ciprod/ama-metrics-arc}"
+SUBSCRIPTION="${ADMIN_SUBSCRIPTION_ID:-b9842c7c-1a38-4385-8f39-a51314758bcf}"
+RESOURCE_AUDIENCE="${RESOURCE_AUDIENCE:-c699bf69-fb1d-4eaf-999b-99e6b2ae4d85}"
+SPN_CLIENT_ID="${SPN_CLIENT_ID:-9a4c55e9-576a-450a-88bd-53bd634db38d}"
+SPN_TENANT_ID="${SPN_TENANT_ID:-72f988bf-86f1-41af-91ab-2d7cd011db47}"
+ARC_API_URL="${ARC_API_URL:-https://eastus2euap.dp.kubernetesconfiguration.azure.com}"
+EXTENSION_NAME="${EXTENSION_NAME:-microsoft.azuremonitor.containers.metrics}"
 
 CANARY_BATCH="\"eastus2euap\",\"centraluseuap\""
 SMALL_REGION="$CANARY_BATCH,\"westcentralus\""
@@ -56,15 +60,15 @@ if [ -z "$CHART_VERSION" ]; then
 fi
 
 echo "Pulling chart from MCR:${REGISTRY_PATH}"
-helm chart pull ${REGISTRY_PATH}:${CHART_VERSION}
+helm pull oci://${REGISTRY_PATH} --version ${CHART_VERSION}
 if [ $? -eq 0 ]; then
-  echo "Pulling chart from MCR:${REGISTRY_PATH}:${CHART_VERSION} completed successfully."
+  echo "Pulling chart from ${REGISTRY_PATH}:${CHART_VERSION} completed successfully."
 else
-  echo "-e error Pulling chart from MCR:${REGISTRY_PATH}:${CHART_VERSION} failed. Please review Ev2 pipeline logs for more details on the error."
+  echo "-e error Pulling chart from ${REGISTRY_PATH}:${CHART_VERSION} failed. Please review Ev2 pipeline logs for more details on the error."
   exit 1
-fi   
+fi
 
-echo "Start arc extension release stage ${RELEASE_STAGE}, REGISTER_REGIONS is REGIONS_BATCH, RELEASE_TRAINS are $RELEASE_TRAINS, PACKAGE_CONFIG_NAME is $PACKAGE_CONFIG_NAME, API_VERSION is $API_VERSION, METHOD is $METHOD"
+echo "Start arc extension release. REGISTER_REGIONS is REGIONS_BATCH, RELEASE_TRAINS are $RELEASE_TRAINS, PACKAGE_CONFIG_NAME is $PACKAGE_CONFIG_NAME, API_VERSION is $API_VERSION, METHOD is $METHOD"
 
 # Create JSON request body
 cat <<EOF > "request.json"
@@ -77,7 +81,7 @@ cat <<EOF > "request.json"
             "Releasetrains": [
                 $RELEASE_TRAINS
             ],
-            "FullPathToHelmChart": "$REGISTRY_PATH",
+            "FullPathToHelmChart": "https://$REGISTRY_PATH",
             "ExtensionUpdateFrequencyInMinutes": 60,
             "IsCustomerHidden": $IS_CUSTOMER_HIDDEN,
             "ReadyforRollout": true,
@@ -89,14 +93,11 @@ cat <<EOF > "request.json"
 EOF
 
 # Send Request
-SUBSCRIPTION=${ADMIN_SUBSCRIPTION_ID}
-RESOURCE_AUDIENCE=${RESOURCE_AUDIENCE}
-
 echo "Request parameter preparation, SUBSCRIPTION is $SUBSCRIPTION, RESOURCE_AUDIENCE is $RESOURCE_AUDIENCE, CHART_VERSION is $CHART_VERSION, SPN_CLIENT_ID is $SPN_CLIENT_ID, SPN_TENANT_ID is $SPN_TENANT_ID"
 
 # MSI is not supported
 echo "Login cli using spn"
-az login --service-principal --username=${SPN_CLIENT_ID} --password=${SPN_SECRET} --tenant=${SPN_TENANT_ID}
+az login --service-principal --username=$SPN_CLIENT_ID --password=${SPN_SECRET} --tenant=$SPN_TENANT_ID
 if [ $? -eq 0 ]; then
   echo "Logged in successfully with spn"
 else
@@ -112,9 +113,6 @@ else
   exit 1
 fi   
 ACCESS_TOKEN=$(echo $ACCESS_TOKEN | tr -d '"' | tr -d '"\r\n')
-
-ARC_API_URL="https://eastus2euap.dp.kubernetesconfiguration.azure.com"
-EXTENSION_NAME="microsoft.azuremonitor.containers.metrics"
 
 echo "start send request"
 az rest --method $METHOD --headers "{\"Authorization\": \"Bearer $ACCESS_TOKEN\", \"Content-Type\": \"application/json\"}" --body @request.json --uri $ARC_API_URL/subscriptions/$SUBSCRIPTION/extensionTypeRegistrations/$EXTENSION_NAME/versions/$CHART_VERSION?api-version=$API_VERSION
