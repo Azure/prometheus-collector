bc76b06f05f9542b54d17644a7e13ac1f5e849b5
Rashmi Chandrashekar <rashmy@microsoft.com>
2023-07-05 10:17:39 -0700

    fix main.sh script syntax error

--- otelcollector/scripts/main.sh
+++ otelcollector/scripts/main.sh
@@ -224,7 +224,7 @@ elif [ -e "/opt/defaultsMergedConfig.yml" ]; then
 
       # echo "running configuration reader"
       # /opt/configurationreader
-elif [ $controllerType != "replicaset" ]
+elif [ $controllerType != "replicaset" ]; then
       # This else block is needed, when there is no custom config mounted as config map or default configs enabled
       echo_error "prom-config-validator::No custom config via configmap or default scrape configs enabled"
       echo "export AZMON_USE_DEFAULT_PROMETHEUS_CONFIG=true" >> ~/.bashrc
