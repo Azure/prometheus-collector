9c3bccc771c4cff3f6b45fc9f213640950a4de43
Kaveesh Dubey <kadubey@microsoft.com>
2023-10-11 11:29:57 -0700

    set mdsd env properly

--- otelcollector/prometheuscollector/main.go
+++ otelcollector/prometheuscollector/main.go
@@ -12,13 +12,12 @@ import (
 )
 
 func main() {
-    // Run inotify as a daemon to track changes to the mounted configmap.
-    runShellCommand("touch", "/opt/inotifyoutput.txt")
-    runShellCommand("inotifywait", "/etc/config/settings", "--daemon", "--recursive", "--outfile", "/opt/inotifyoutput.txt", "--event", "create,delete", "--format", "'%e : %T'", "--timefmt", "'+%s'")
-
-
+	mac := os.Getenv("MAC")
     controllerType := os.Getenv("controllerType")
 	clusterOverride := os.Getenv("CLUSTER_OVERRIDE")
+	cluster := os.Getenv("CLUSTER")
+	aksRegion := os.Getenv("AKSREGION")
+	mdsdLog := os.Getenv("MDSD_LOG")
 
 	var meConfigFile string
 
@@ -37,7 +36,7 @@ func main() {
 	}
 	fmt.Println("meConfigFile:", meConfigFile)
 
-    if os.Getenv("MAC") == "true" {
+    if mac == "true" {
 		// Wait for addon-token-adapter to be healthy
 		tokenAdapterWaitSecs := 60
 		waitedSecsSoFar := 1
@@ -67,79 +66,31 @@ func main() {
 		}
     }
 
-	fluentBitConfigFile := os.Getenv("fluentBitConfigFile")
-	mac := os.Getenv("MAC")
-	cluster := os.Getenv("CLUSTER")
-	aksRegion := os.Getenv("AKSREGION")
-	mdsdLog := os.Getenv("MDSD_LOG")
-
 	// Set environment variables
 	os.Setenv("ME_CONFIG_FILE", meConfigFile)
-	os.Setenv("FLUENT_BIT_CONFIG_FILE", fluentBitConfigFile)
+	os.Setenv("customResourceId", cluster)
 
-	// Append environment variable assignments 
-	appendToEtcEnvironment("ME_CONFIG_FILE", meConfigFile)
-	appendToEtcEnvironment("FLUENT_BIT_CONFIG_FILE", fluentBitConfigFile)
+	trimmedRegion := strings.ReplaceAll(aksRegion, " ", "")
+	trimmedRegion = strings.ToLower(trimmedRegion)
+	os.Setenv("customRegion", trimmedRegion)
 
-	if mac != "true" {
-		if cluster == "" {
-			fmt.Printf("CLUSTER is empty or not set. Using %s as CLUSTER\n", os.Getenv("NODE_NAME"))
-			os.Setenv("customResourceId", os.Getenv("NODE_NAME"))
-		} else {
-			fmt.Printf("CLUSTER is set.\n")
-			os.Setenv("customResourceId", cluster)
-		}
-
-		// Append customResourceId 
-		appendToEtcEnvironment("customResourceId", os.Getenv("customResourceId"))
-
-		// Make a copy of the mounted akv directory
-		copyAkvDirectory("/etc/config/settings/akv", "/opt/akv-copy")
-
-		decodeLocation := "/opt/akv/decoded"
-		ENCODEDFILES := listFiles("/etc/config/settings/akv")
-
-		// Decode and set environment variables
-		decodeAndSetEnvVars(decodeLocation, ENCODEDFILES)
-
-		// Append AZMON_METRIC_ACCOUNTS_AKV_FILES
-		appendToEtcEnvironment("AZMON_METRIC_ACCOUNTS_AKV_FILES", os.Getenv("AZMON_METRIC_ACCOUNTS_AKV_FILES"))
-
-		fmt.Println("Starting metricsextension")
-
-		// Start MetricsExtension with appropriate options
-		startMetricsExtension()
-
-	} else {
-		os.Setenv("customResourceId", cluster)
-		appendToEtcEnvironment("customResourceId", os.Getenv("customResourceId"))
-
-		trimmedRegion := strings.ReplaceAll(aksRegion, " ", "")
-		trimmedRegion = strings.ToLower(trimmedRegion)
-		os.Setenv("customRegion", trimmedRegion)
-		appendToEtcEnvironment("customRegion", os.Getenv("customRegion"))
-
-		fmt.Println("Waiting for 10s for token adapter sidecar to be up and running so that it can start serving IMDS requests")
-		time.Sleep(10 * time.Second)
-
-		fmt.Println("Setting env variables from envmdsd file for MDSD")
-		setEnvVarsFromEnvMdsdFile("/etc/mdsd.d/envmdsd")
-
-		fmt.Println("Starting MDSD")
-		startMdsd(mdsdLog)
-
-		fmt.Print("MDSD_VERSION=")
-		printMdsdVersion()
-
-		fmt.Println("Waiting for 30s for MDSD to get the config and put them in place for ME")
-		time.Sleep(30 * time.Second)
-
-		fmt.Println("Reading me config file as a string for configOverrides parameter")
-		meConfigString := readMeConfigFileAsString(meConfigFile)
-
-		fmt.Println("Starting metricsextension with config overrides")
-		startMetricsExtensionWithConfigOverrides(meConfigString)
-	}
+	fmt.Println("Waiting for 10s for token adapter sidecar to be up and running so that it can start serving IMDS requests")
+	time.Sleep(10 * time.Second)
+	
+	fmt.Println("Starting MDSD")
+	startMdsd(mdsdLog)
+	
+	fmt.Print("MDSD_VERSION=")
+	printMdsdVersion()
+	
+	fmt.Println("Waiting for 30s for MDSD to get the config and put them in place for ME")
+	time.Sleep(30 * time.Second)
+	
+	fmt.Println("Reading me config file as a string for configOverrides parameter")
+	meConfigString := readMeConfigFileAsString(meConfigFile)
+	
+	fmt.Println("Starting metricsextension with config overrides")
+	startMetricsExtensionWithConfigOverrides(meConfigString)
 
     // Get ME version
 	meVersion, err := readVersionFile("/opt/metricsextversion.txt")
@@ -243,13 +194,6 @@ func main() {
     http.ListenAndServe(":8080", nil)
 }
 
-func runShellCommand(command string, args ...string) {
-    cmd := exec.Command(command, args...)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Run()
-}
-
 func healthHandler(w http.ResponseWriter, r *http.Request) {
     prometheuscollectorRunning := isProcessRunning("prometheuscollector")
 
@@ -268,104 +212,14 @@ func isProcessRunning(processName string) bool {
     return err == nil
 }
 
-func appendToEtcEnvironment(variableName, variableValue string) error {
-    // Check if the variable already exists in /etc/environment
-    cmd := exec.Command("grep", variableName, "/etc/environment")
-    cmd.Stderr = os.Stderr
-    err := cmd.Run()
-    if err == nil {
-        // Variable already exists, so update its value
-        updateCmd := exec.Command("sed", "-i", fmt.Sprintf("/^%s=/s/=.*/=%s/", variableName, variableValue), "/etc/environment")
-        updateCmd.Stderr = os.Stderr
-        return updateCmd.Run()
-    }
-
-    // Variable doesn't exist, so append it to /etc/environment
-    appendCmd := exec.Command("echo", fmt.Sprintf("%s=%s | tee -a /etc/environment", variableName, variableValue))
-    appendCmd.Stderr = os.Stderr
-    return appendCmd.Run()
-}
-
-func copyAkvDirectory(sourceDir, destDir string) {
-	cmd := exec.Command("cp", "-r", sourceDir, destDir)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Error copying AKV directory: %v\n", err)
-	}
-}
-
-func listFiles(directory string) []string {
-	files, err := ioutil.ReadDir(directory)
-	if err != nil {
-		fmt.Printf("Error listing files in directory: %v\n", err)
-		return nil
-	}
-
-	var fileNames []string
-	for _, file := range files {
-		fileNames = append(fileNames, directory+"/"+file.Name())
-	}
-	return fileNames
-}
-
-func decodeAndSetEnvVars(decodeLocation string, encodedFiles []string) {
-	os.MkdirAll(decodeLocation, os.ModePerm)
-	var decodedFiles []string
-
-	for _, encodedFile := range encodedFiles {
-		baseName := encodedFile[strings.LastIndex(encodedFile, "/")+1:]
-		fileName := decodeLocation + "/" + baseName
-		decodedFiles = append(decodedFiles, fileName)
-
-		cmd := exec.Command("base64", "-d", encodedFile)
-		decodedOutput, err := cmd.Output()
-		if err != nil {
-			fmt.Printf("Error decoding file: %v\n", err)
-		}
-
-		err = ioutil.WriteFile(fileName, decodedOutput, os.ModePerm)
-		if err != nil {
-			fmt.Printf("Error writing decoded file: %v\n", err)
-		}
-	}
-
-	// Combine decoded file paths into a single string
-	decodedFilesStr := strings.Join(decodedFiles, ":")
-
-	os.Setenv("AZMON_METRIC_ACCOUNTS_AKV_FILES", decodedFilesStr)
-}
-
-
-func startMetricsExtension() {
-	cmd := exec.Command(
-		"/usr/sbin/MetricsExtension",
-		"-Logger", "File",
-		"-LogLevel", "Info",
-		"-DataDirectory", "/opt/MetricsExtensionData",
-		"-Input", "otlp_grpc_prom",
-		"-PfxFile", os.Getenv("AZMON_METRIC_ACCOUNTS_AKV_FILES"),
-		"-MonitoringAccount", os.Getenv("AZMON_DEFAULT_METRIC_ACCOUNT_NAME"),
-		"-ConfigOverridesFilePath", os.Getenv("ME_CONFIG_FILE"),
-	)
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "customResourceId=" + os.Getenv("CLUSTER"))
-	cmd.Env = append(cmd.Env, "customRegion=" + os.Getenv("AKSREGION"))
-	fmt.Println("cmd.Env for MetricsExtension")
-	fmt.Println(cmd.Env)
-	err := cmd.Start()
-	if err != nil {
-		fmt.Printf("Error starting MetricsExtension: %v\n", err)
-	}
-}
-
-func setEnvVarsFromEnvMdsdFile(envMdsdFile string) {
+func readEnvVarsFromEnvMdsdFile(envMdsdFile string) ([]string, error) {
 	content, err := ioutil.ReadFile(envMdsdFile)
 	if err != nil {
-		fmt.Printf("Error reading envmdsd file: %v\n", err)
-		return
+		return nil, err
 	}
 
 	lines := strings.Split(string(content), "\n")
+	var envVars []string
 	for _, line := range lines {
 		// Trim any leading/trailing spaces and ignore empty lines
 		line = strings.TrimSpace(line)
@@ -376,15 +230,21 @@ func setEnvVarsFromEnvMdsdFile(envMdsdFile string) {
 		parts := strings.SplitN(line, "=", 2)
 		if len(parts) == 2 {
 			name, value := parts[0], parts[1]
-			err := os.Setenv(name, value)
-			if err != nil {
-				fmt.Printf("Error setting environment variable from envmdsd file: %v\n", err)
-			}
+			envVars = append(envVars, name+"="+value)
 		}
 	}
-}
 
+	return envVars, nil
+}
 func startMdsd(mdsdLog string) {
+	fmt.Println("Setting env variables from envmdsd file for MDSD")
+	// Read environment variables from the envmdsd file
+	envVarsFromEnvMdsd, err := readEnvVarsFromEnvMdsdFile("/etc/mdsd.d/envmdsd")
+	if err != nil {
+		fmt.Printf("Error reading envmdsd file: %v\n", err)
+		return
+	}
+
 	cmd := exec.Command(
 		"mdsd",
 		"-a", "-A",
@@ -392,14 +252,13 @@ func startMdsd(mdsdLog string) {
 		"-w", mdsdLog+"/mdsd.warn",
 		"-o", mdsdLog+"/mdsd.info",
 		"-q", mdsdLog+"/mdsd.qos",
-		// "2>>", "/dev/null",
 	)
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "customResourceId=" + os.Getenv("CLUSTER"))
-	cmd.Env = append(cmd.Env, "customRegion=" + os.Getenv("AKSREGION"))
+	// Set the environment variables in cmd.Env
+	cmd.Env = append(os.Environ(), envVarsFromEnvMdsd...)
+
 	fmt.Println("cmd.Env for MDSD")
 	fmt.Println(cmd.Env)
-	err := cmd.Start()
+	err = cmd.Start()
 	if err != nil {
 		fmt.Printf("Error starting MDSD: %v\n", err)
 	}
