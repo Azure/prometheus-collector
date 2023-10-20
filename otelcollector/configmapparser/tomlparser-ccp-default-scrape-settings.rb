2e6dc47d49a7204375be0637a16e7b80c578778a
Kaveesh Dubey <kadubey@microsoft.com>
2023-10-20 10:03:05 -0700

    bug

--- otelcollector/configmapparser/tomlparser-ccp-default-scrape-settings.rb
+++ otelcollector/configmapparser/tomlparser-ccp-default-scrape-settings.rb
@@ -37,24 +37,24 @@ end
 # Use the ruby structure created after config parsing to set the right values to be used for otel collector settings
 def populateSettingValuesFromConfigMap(parsedConfig)
   begin
-    if !parsedConfig[:controlplane-kube-controller-manager].nil?
-      @controlplane_kube_controller_manager_enabled = parsedConfig[:controlplane-kube-controller-manager]
+    if !parsedConfig[:controlplane_kube_controller_manager].nil?
+      @controlplane_kube_controller_manager_enabled = parsedConfig[:controlplane_kube_controller_manager]
       puts "config::Using configmap scrape settings for controlplane-kube-controller-manager: #{@controlplane_kube_controller_manager_enabled}"
     end
-    if !parsedConfig[:controlplane-kube-scheduler].nil?
-      @controlplane_kube_scheduler_enabled = parsedConfig[:controlplane-kube-scheduler]
+    if !parsedConfig[:controlplane_kube_scheduler].nil?
+      @controlplane_kube_scheduler_enabled = parsedConfig[:controlplane_kube_scheduler]
       puts "config::Using configmap scrape settings for controlplane-kube-scheduler: #{@controlplane_kube_scheduler_enabled}"
     end
-    if !parsedConfig[:controlplane-apiserver].nil?
-      @controlplane_apiserver_enabled = parsedConfig[:controlplane-apiserver]
+    if !parsedConfig[:controlplane_apiserver].nil?
+      @controlplane_apiserver_enabled = parsedConfig[:controlplane_apiserver]
       puts "config::Using configmap scrape settings for controlplane-apiserver: #{@controlplane_apiserver_enabled}"
     end
-    if !parsedConfig[:controlplane-cluster-autoscaler].nil?
-      @controlplane_cluster_autoscaler_enabled = parsedConfig[:controlplane-cluster-autoscaler]
+    if !parsedConfig[:controlplane_cluster_autoscaler].nil?
+      @controlplane_cluster_autoscaler_enabled = parsedConfig[:controlplane_cluster_autoscaler]
       puts "config::Using configmap scrape settings for controlplane-cluster-autoscaler: #{@controlplane_cluster_autoscaler_enabled}"
     end
-    if !parsedConfig[:controlplane-etcd].nil?
-      @controlplane_etcd_enabled = parsedConfig[:controlplane-etcd]
+    if !parsedConfig[:controlplane_etcd].nil?
+      @controlplane_etcd_enabled = parsedConfig[:controlplane_etcd]
       puts "config::Using configmap scrape settings for controlplane-etcd: #{@controlplane_etcd_enabled}"
     end
 
