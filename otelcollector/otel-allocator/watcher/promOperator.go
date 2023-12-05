0ca189cc802d8be4aa08e02a8cba23faf8d21f56
Rashmi Chandrashekar <rashmy@microsoft.com>
2023-12-05 11:35:13 -0800

    add warning

--- otelcollector/otel-allocator/watcher/promOperator.go
+++ otelcollector/otel-allocator/watcher/promOperator.go
@@ -102,7 +102,7 @@ func NewPrometheusCRWatcher(ctx context.Context, logger logr.Logger, cfg allocat
 
 	nsMonInf, err := getNamespaceInformer(ctx, map[string]struct{}{v1.NamespaceAll: {}}, promOperatorLogger, clientset, operatorMetrics)
 	if err != nil {
-		return nil, err
+		logger.Warn(err, "Failed to create namespace informer in CRD watcher")
 	}
 
 	resourceSelector := prometheus.NewResourceSelector(promOperatorLogger, prom, store, nsMonInf, operatorMetrics)
