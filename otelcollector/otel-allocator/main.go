1fa1d2f50030c7097ddd4accf5eef01c9246ac0b
Rashmi Chandrashekar <rashmy@microsoft.com>
2024-01-03 16:01:48 -0800

    add eula statement

--- otelcollector/otel-allocator/main.go
+++ otelcollector/otel-allocator/main.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	gokitlog "github.com/go-kit/log"
@@ -65,6 +66,13 @@ func main() {
 		interrupts      = make(chan os.Signal, 1)
 		errChan         = make(chan error)
 	)
+
+	// EULA statement is required for Arc extension
+	clusterResourceId := os.Getenv("CLUSTER")
+	if strings.EqualFold(clusterResourceId, "connectedclusters") {
+		setupLog.Info("MICROSOFT SOFTWARE LICENSE TERMS\n\nMICROSOFT Azure Arc-enabled Kubernetes\n\nThis software is licensed to you as part of your or your company's subscription license for Microsoft Azure Services. You may only use the software with Microsoft Azure Services and subject to the terms and conditions of the agreement under which you obtained Microsoft Azure Services. If you do not have an active subscription license for Microsoft Azure Services, you may not use the software. Microsoft Azure Legal Information: https://azure.microsoft.com/en-us/support/legal/")
+	}
+
 	cfg, configFilePath, err := config.Load()
 	if err != nil {
 		fmt.Printf("Failed to load config: %v", err)
