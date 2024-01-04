cdd6a16110f27819fe1231b67058d4ae19cd46e5
rashmichandrashekar <rashmy@microsoft.com>
2024-01-04 14:13:58 -0800

    Upgrade collector, targetallocator and prometheus operator to latest versions (0.90, 0.90 and 0.69.1) (#706)

--- otelcollector/prometheusreceiver/factory_test.go
+++ otelcollector/prometheusreceiver/factory_test.go
@@ -15,7 +15,7 @@ import (
 	"go.opentelemetry.io/collector/confmap/confmaptest"
 	"go.opentelemetry.io/collector/receiver/receivertest"
 
-	"github.com/gracewehner/prometheusreceiver/internal/metadata"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver/internal/metadata"
 )
 
 func TestCreateDefaultConfig(t *testing.T) {
