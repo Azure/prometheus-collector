d19a8201e8a8164f319c6c3e4844182fb8cf18a2
Grace Wehner <grace.wehner@microsoft.com>
2024-01-29 11:26:20 -0800

    fix syntax

--- otelcollector/test/utils/constants.go
+++ otelcollector/test/utils/constants.go
@@ -4,6 +4,6 @@ var(
 	// Slices can't be constants
 	LogLineErrorsToExclude = [...]string{
 		"\"filepath\":\"/MetricsExtensionConsoleDebugLog.log\"",
-		"create or renew cluster identity error"
+		"create or renew cluster identity error",
 	}
 )
