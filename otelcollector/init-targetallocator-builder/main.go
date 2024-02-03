57708ff9327ed4901c5e7399459e024cb654b864
Rashmi Chandrashekar <rashmy@microsoft.com>
2024-02-02 17:36:33 -0800

    changes

--- otelcollector/init-targetallocator-builder/main.go
+++ otelcollector/init-targetallocator-builder/main.go
@@ -2,22 +2,17 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
-	"os/exec"
+	"time"
 )
-
 
 func main() {
 	// Waiting until config file is created by the sidecar
-	while not os.path.exists("/conf/targetallocator.yaml"):
-    time.sleep(1)
+	if _, err := os.Stat("/conf/targetallocator.yaml"); err == nil {
+		fmt.Println("Config file created at /conf/targetallocator.yaml")
+		os.Exit(0)
 
-	fmt.Println("Config file created at /conf/targetallocator.yaml")
-
-	os.Exit(0)
+	} else {
+		time.Sleep(1 * time.Second)
+	}
 }
-
-
