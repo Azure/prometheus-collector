d48f7267d8cefaa81b0e79c8e4a6ccd60b6c9428
Rashmi Chandrashekar <rashmy@microsoft.com>
2024-02-20 16:38:02 -0800

    logging

--- otelcollector/otel-allocator/target/discovery.go
+++ otelcollector/otel-allocator/target/discovery.go
@@ -93,6 +93,8 @@ func (m *Discoverer) ApplyConfig(source allocatorWatcher.EventSource, cfg *confi
 	// Otherwise skip updating scrape configs.
 	if m.scrapeConfigsUpdater != nil && m.scrapeConfigsHash != hash {
 		m.log.Info("Rashmi-hash is different", "source", source.String())
+		m.log.Info("Rashmi-ols hash", "old hash", m.scrapeConfigsHash.String())
+		m.log.Info("Rashmi-new hash", "new hash", hash.String())
 		err := m.scrapeConfigsUpdater.UpdateScrapeConfigResponse(jobToScrapeConfig)
 		if err != nil {
 			return err
