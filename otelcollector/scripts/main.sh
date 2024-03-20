c96b3bb46e4e4fc7cde92c22627d65cfbefff47e
Kaveesh Dubey <kadubey@microsoft.com>
2024-03-19 17:52:08 -0700

    fix cloud name

--- otelcollector/scripts/main.sh
+++ otelcollector/scripts/main.sh
@@ -20,7 +20,7 @@ echo_var "CLUSTER" "$CLUSTER"
 customEnvironment_lower=$(echo "$customEnvironment" | tr '[:upper:]' '[:lower:]')
 if [ "$customEnvironment_lower" == "azurepubliccloud" ]; then
   encodedaikey="MWNkYTMxMTItYWY1Ni00ZmNiLWI4MDQtZjg5NDVhYTFjYjMy"
-elif [ "$customEnvironment_lower" == "azureusgovernment" ]; then
+elif [ "$customEnvironment_lower" == "azureusgovernmentcloud" ]; then
   # encodedaikey="ZmRjMTE0MmUtY2U0YS1mNTFmLWE4M2EtODBjM2ZjNDYwNGE5"
   # aiendpoint="https://dc.applicationinsights.us/v2/track"
   encodedaikey="OWNmYzNmZDEtMzFiZS1mOWE4LTgzMmYtMjNiYzIzNmQ0MWIy"
