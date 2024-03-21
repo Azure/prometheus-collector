5e3d626236b98dcf8f233fcde4bf4a7d52bada3b
Grace Wehner <grace.wehner@microsoft.com>
2024-03-21 14:07:23 -0700

    remove dependencies for operator tests

--- otelcollector/test/operator/operator_suite_test.go
+++ otelcollector/test/operator/operator_suite_test.go
@@ -1,23 +1,11 @@
 package operator
 
 import (
-	"prometheus-collector/otelcollector/test/utils"
 	"testing"
-
-	"k8s.io/client-go/kubernetes"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	promOperatorClient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
-	"k8s.io/client-go/rest"
 )
-
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
-var K8sClient 	*kubernetes.Clientset
-var Cfg       	*rest.Config
-var PromClient 	promOperatorClient.Interface
 
 /*
  * These tests MUST be run with the flag:
@@ -31,11 +19,6 @@ func TestOperator(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-  var err error
-  K8sClient, Cfg, err = utils.SetupKubernetesClient()
-  Expect(err).NotTo(HaveOccurred())
-	PromClient, err = promOperatorClient.NewForConfig(Cfg)
-  Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
--- otelcollector/test/operator/operator_test.go
+++ otelcollector/test/operator/operator_test.go
@@ -1,40 +1,14 @@
 package operator
 
 import (
-	"context"
-	"fmt"
-
-	"prometheus-collector/otelcollector/test/utils"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("Calling the API server using the Prometheus Operator client", Label(utils.OperatorLabel), func() {
+var _ = Describe("Calling the API server using the Prometheus Operator client", func() {
   It("should get the pod monitor custom resources", func() {
-    podMonitors, err := PromClient.MonitoringV1().PodMonitors("").List(context.Background(), metav1.ListOptions{})
-    Expect(err).NotTo(HaveOccurred())
-
-    fmt.Printf("Found %d Pod Monitors:\n", len(podMonitors.Items))
-    for _, pm := range podMonitors.Items {
-      fmt.Printf("- Name: %s\n", pm.Name)
-      fmt.Printf("  Namespace: %s\n", pm.Namespace)
-      fmt.Printf("  Labels: %v\n", pm.Labels)
-      fmt.Printf("  Spec: %v\n", pm.Spec)
-    }
   })
 
   It("should get the service monitor custom resources", func() {
-    serviceMonitors, err := PromClient.MonitoringV1().ServiceMonitors("").List(context.Background(), metav1.ListOptions{})
-    Expect(err).NotTo(HaveOccurred())
-
-    fmt.Printf("Found %d Service Monitors:\n", len(serviceMonitors.Items))
-    for _, sm := range serviceMonitors.Items {
-      fmt.Printf("- Name: %s\n", sm.Name)
-      fmt.Printf("  Namespace: %s\n", sm.Namespace)
-      fmt.Printf("  Labels: %v\n", sm.Labels)
-      fmt.Printf("  Spec: %v\n", sm.Spec)
-    }
   })
 })
