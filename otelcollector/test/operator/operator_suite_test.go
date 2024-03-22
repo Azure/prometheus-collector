ee1e3f8d11cd6eafcc9c6f87e043848e17ab88cf
Grace Wehner <grace.wehner@microsoft.com>
2024-03-22 11:53:06 -0700

    rename back to operator

--- otelcollector/test/operator/operator_suite_test.go
+++ otelcollector/test/operator/operator_suite_test.go
@@ -1,65 +1,41 @@
 package operator
 
 import (
-	//"prometheus-collector/otelcollector/test/utils"
-
 	"testing"
+
+	"k8s.io/client-go/kubernetes"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	//promOperatorClient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
-
-	"k8s.io/client-go/kubernetes"
+	promOperatorClient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
 	"k8s.io/client-go/rest"
+
+	"prometheus-collector/otelcollector/test/utils"
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
-
-//var K8sClient 	*kubernetes.Clientset
-//var Cfg       	*rest.Config
-//var PromClient 	promOperatorClient.Interface
+var K8sClient 	*kubernetes.Clientset
+var Cfg       	*rest.Config
+var PromClient  promOperatorClient.Interface
 
 /*
  * These tests MUST be run with the flag:
  * -ldflags="-s -X github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring.GroupName=azmonitoring.coreos.com"
  * in order for the prometheus-operator package to get CRs using our custom API group name.
  */
-func TestOperator(t *testing.T) {
+func TestPrometheusOperatorCRs(t *testing.T) {
   RegisterFailHandler(Fail)
 
-  RunSpecs(t, "Operator Test Suite")
+  RunSpecs(t, "Prometheus Operator CRs Test Suite")
 }
 
 var _ = BeforeSuite(func() {
   var err error
-  //K8sClient, Cfg, err = utils.SetupKubernetesClient()
-	_, _, err = SetupKubernetesClient()
+  K8sClient, Cfg, err = utils.SetupKubernetesClient()
   Expect(err).NotTo(HaveOccurred())
-	//PromClient, err = promOperatorClient.NewForConfig(Cfg)
-  //Expect(err).NotTo(HaveOccurred())
+	PromClient, err = promOperatorClient.NewForConfig(Cfg)
+  Expect(err).NotTo(HaveOccurred())
 })
 
 var _ = AfterSuite(func() {
   By("tearing down the test environment")
 })
-
-/*
- * Returns the Kubernetes API client and cluster configuration.
- * The function will first check if a kubeconfig file is present in the user's home directory, for running the tests locally.
- * If the file is not found, it will assume the tests are running in a Kubernetes cluster and use the in-cluster configuration.
- */
- func SetupKubernetesClient() (*kubernetes.Clientset, *rest.Config, error) {
-	cfg, err := rest.InClusterConfig()
-	if err != nil {
-		return nil, nil, err
-	}
-  
-  client, err := kubernetes.NewForConfig(cfg)
-  if err != nil {
-    return nil, nil, err
-  }
-
-  return client, cfg, nil
-}
