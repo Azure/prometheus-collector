ae8f29e9447340a91eb42b4a69dedcedf08f2c4a
Grace Wehner <grace.wehner@microsoft.com>
2024-03-22 10:26:37 -0700

    add in client-go

--- otelcollector/test/crds/crd_suite_test.go
+++ otelcollector/test/crds/crd_suite_test.go
@@ -1,18 +1,22 @@
 package crds
 
 import (
-	"prometheus-collector/otelcollector/test/utils"
 	"testing"
+
+	"k8s.io/client-go/kubernetes"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+
+	"prometheus-collector/otelcollector/test/utils"
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-// var K8sClient 	*kubernetes.Clientset
-// var Cfg       	*rest.Config
+var K8sClient 	*kubernetes.Clientset
+var Cfg       	*rest.Config
 
 func TestCRDs(t *testing.T) {
   RegisterFailHandler(Fail)
@@ -22,7 +26,7 @@ func TestCRDs(t *testing.T) {
 
 var _ = BeforeSuite(func() {
   var err error
-  _, _, err = utils.SetupKubernetesClient()
+  K8sClient, Cfg, err = utils.SetupKubernetesClient()
   Expect(err).NotTo(HaveOccurred())
 })
 
--- otelcollector/test/crds/go.mod
+++ otelcollector/test/crds/go.mod
@@ -7,6 +7,7 @@ replace prometheus-collector/otelcollector/test/utils => ../utils
 require (
 	github.com/onsi/ginkgo/v2 v2.13.1
 	github.com/onsi/gomega v1.30.0
+	k8s.io/client-go v0.28.4
 	prometheus-collector/otelcollector/test/utils v0.0.0-00010101000000-000000000000
 )
 
@@ -51,7 +52,6 @@ require (
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/api v0.28.4 // indirect
 	k8s.io/apimachinery v0.28.4 // indirect
-	k8s.io/client-go v0.28.4 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
