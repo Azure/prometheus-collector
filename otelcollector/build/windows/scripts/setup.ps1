42cb10ed96438bac8800db2d781adc6bba4404ab
vishwanath <visnara@microsoft.com>
2024-04-01 13:19:37 -0700

    Upgrade ME (linux & windows) to 2.2024.328.1744

--- otelcollector/build/windows/scripts/setup.ps1
+++ otelcollector/build/windows/scripts/setup.ps1
@@ -15,8 +15,8 @@ New-Item -Type Directory -Path /etc/genevamonitoringagent
 ############################################################################################
 Write-Host ('Installing Metrics Extension');
 try {
-    Invoke-WebRequest -Uri "https://github.com/Azure/prometheus-collector/releases/download/Prometheus-MetricsExtension-Windows-3.15.2023/MdmMetricsExtension.2.2023.224.2214.nupkg" -OutFile /installation/ME/mdmmetricsextension.2.2023.224.2214.zip
-    Expand-Archive -Path /installation/ME/mdmmetricsextension.2.2023.224.2214.zip -Destination /installation/ME/
+    Invoke-WebRequest -Uri "https://github.com/Azure/prometheus-collector/releases/download/metricsext2-2.2024.328.1744/MdmMetricsExtension.2.2024.328.1744.nupkg" -OutFile /installation/ME/mdmmetricsextension.2.2023.224.2214.zip
+    Expand-Archive -Path /installation/ME/mdmmetricsextension.2.2024.328.1744.zip -Destination /installation/ME/
     Move-Item /installation/ME/MetricsExtension /opt/metricextension/
 }
 catch {
