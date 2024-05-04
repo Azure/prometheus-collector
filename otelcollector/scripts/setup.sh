07438b2767522aa27a556d6ec46a667bafc74382
Vishwanath <visnara@microsoft.com>
2024-05-03 19:54:38 -0700

    Upgrade ME (for hdinsights OOM bug) (#877)
    * Upgrade ME for both linux & windows (this fixes the HD insights OOM
    issue) ME version from: metricsext2-2.2024.328.1744 to:
    metricsext2-2.2024.419.1535
    * Up version for the release
    * Update release notes
    * Update .trivyignore for new CVEs (need to be fixed in next release)
    
    
    There is no significant difference before v after in either cpu or mem
    usage for ds (linux & windows) & rs (see below) , and also successful
    metric ingestion volume is the same , with no drops in ME --
    
    ds (linux) --
    
    <img width="1851" alt="image"
    src="https://github.com/Azure/prometheus-collector/assets/10353076/1adc92cb-5ea9-46cf-9a43-642f13e4cf5f">
    
    
    
    ds (windows) --
    
    <img width="1862" alt="image"
    src="https://github.com/Azure/prometheus-collector/assets/10353076/c991f778-0fb1-4cdc-89e4-56ef427e144b">
    
    
    
    rs --
    
    <img width="1861" alt="image"
    src="https://github.com/Azure/prometheus-collector/assets/10353076/e99fbd95-2002-42b1-be6f-745b273bc895">
    
    
    
    [comment]: # (The below checklist is for PRs adding new features. If a
    box is not checked, add a reason why it's not needed.)
    # New Feature Checklist
    
    - [ ] List telemetry added about the feature.
    - [ ] Link to the one-pager about the feature.
    - [ ] List any tasks necessary for release (3P docs, AKS RP chart
    changes, etc.) after merging the PR.
    - [ ] Attach results of scale and perf testing.
    
    [comment]: # (The below checklist is for code changes. Not all boxes
    necessarily need to be checked. Build, doc, and template changes do not
    need to fill out the checklist.)
    # Tests Checklist
    
    - [ ] Have end-to-end Ginkgo tests been run on your cluster and passed?
    To bootstrap your cluster to run the tests, follow [these
    instructions](/otelcollector/test/README.md#bootstrap-a-dev-cluster-to-run-ginkgo-tests).
      - Labels used when running the tests on your cluster:
        - [ ] `operator`
        - [ ] `windows`
        - [ ] `arm64`
        - [ ] `arc-extension`
    - [ ] Have new tests been added? For features, have tests been added for
    this feature? For fixes, is there a test that could have caught this
    issue and could validate that the fix works?
      - [ ] Is a new scrape job needed?
    - [ ] The scrape job was added to the folder
    [test-cluster-yamls](/otelcollector/test/test-cluster-yamls/) in the
    correct configmap or as a CR.
      - [ ] Was a new test label added?
    - [ ] A string constant for the label was added to
    [constants.go](/otelcollector/test/utils/constants.go).
    - [ ] The label and description was added to the [test
    README](/otelcollector/test/README.md).
    - [ ] The label was added to this [PR
    checklist](/.github/pull_request_template).
    - [ ] The label was added as needed to
    [testkube-test-crs.yaml](/otelcollector/test/testkube/testkube-test-crs.yaml).
      - [ ] Are additional API server permissions needed for the new tests?
    - [ ] These permissions have been added to
    [api-server-permissions.yaml](/otelcollector/test/testkube/api-server-permissions.yaml).
      - [ ] Was a new test suite (a new folder under `/tests`) added?
    - [ ] The new test suite is included in
    [testkube-test-crs.yaml](/otelcollector/test/testkube/testkube-test-crs.yaml).

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -59,7 +59,7 @@ cp /etc/cron.daily/logrotate /etc/cron.hourly/
 
 # Install ME
 echo "Installing Metrics Extension..."
-sudo tdnf install -y metricsext2-2.2024.328.1744
+sudo tdnf install -y metricsext2-2.2024.419.1535
 sudo tdnf list installed | grep metricsext2 | awk '{print $2}' > metricsextversion.txt
 
 # tdnf does not have an autoremove feature. Only necessary packages are copied over to distroless build. Below reduces the image size if using non-distroless
