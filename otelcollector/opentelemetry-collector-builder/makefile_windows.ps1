9ad37463f1aa8d31063e555140753d9adb2dc7de
Kaveesh Dubey <kadubey@microsoft.com>
2024-08-28 16:26:16 -0700

    build dockerfile

--- otelcollector/opentelemetry-collector-builder/makefile_windows.ps1
+++ otelcollector/opentelemetry-collector-builder/makefile_windows.ps1
@@ -37,3 +37,40 @@ Set-Location ..
 Set-Location opentelemetry-collector-builder
 
 Write-Output "FINISHED building promconfigvalidator"
+
+Set-Location ..
+Set-Location otelcollector
+Set-Location main
+
+# Create directories
+New-Item -Path "./shared/configmap/mp/" -ItemType Directory -Force
+# New-Item -Path "./main/" -ItemType Directory -Force
+
+# Copy shared Go files
+Copy-Item -Path "../shared/*.go" -Destination "./main/shared/"
+Copy-Item -Path "./shared/go.mod" -Destination "./main/shared/"
+Copy-Item -Path "./shared/go.sum" -Destination "./main/shared/"
+Copy-Item -Path "../shared/configmap/mp/*.go" -Destination "./main/shared/configmap/mp/"
+Copy-Item -Path "./shared/configmap/mp/go.mod" -Destination "./main/shared/configmap/mp/"
+Copy-Item -Path "./shared/configmap/mp/go.sum" -Destination "./main/shared/configmap/mp/"
+
+# Copy main Go files
+Copy-Item -Path "./main/*.go" -Destination "./main/"
+Copy-Item -Path "./go.mod" -Destination "./main/"
+Copy-Item -Path "./go.sum" -Destination "./main/"
+
+
+# Print Go version
+go version
+
+# Download Go dependencies
+go mod download
+
+go build -o "main" "./main.go"
+
+Write-Output "Build main executable completed"
+
+Set-Location ..
+Set-Location ..
+Set-Location opentelemetry-collector-builder
+
