de843bbeb98f010872a08127ec89de9b11b31cfe
Kaveesh Dubey <kadubey@microsoft.com>
2024-09-04 11:48:07 -0700

    uncomment

--- otelcollector/opentelemetry-collector-builder/makefile_windows.ps1
+++ otelcollector/opentelemetry-collector-builder/makefile_windows.ps1
@@ -1,42 +1,42 @@
 # Temporary method to build the OTL collector and fluent-bit for windows
 
 # building otelcollector
-# Write-Output "building otelcollector"
-# if (Test-Path "otelcollector.exe") {
-#     Remove-Item .\otelcollector.exe
-# }
-# go get
-# go build -o otelcollector.exe .
+Write-Output "building otelcollector"
+if (Test-Path "otelcollector.exe") {
+    Remove-Item .\otelcollector.exe
+}
+go get
+go build -o otelcollector.exe .
 
-# Write-Output "FINISHED building otelcollector"
+Write-Output "FINISHED building otelcollector"
 
-# # building fluent-bit plugin
+# building fluent-bit plugin
 
-# Write-Output "building fluent-bit plugin"
+Write-Output "building fluent-bit plugin"
 
-# Set-Location ..
-# Set-Location fluent-bit
-# Set-Location src
+Set-Location ..
+Set-Location fluent-bit
+Set-Location src
 
-# .\makefile_windows.ps1
+.\makefile_windows.ps1
 
-# Set-Location ..
-# Set-Location ..
-# Set-Location opentelemetry-collector-builder
+Set-Location ..
+Set-Location ..
+Set-Location opentelemetry-collector-builder
 
-# Write-Output "FINISHED building fluent-bit plugin"
+Write-Output "FINISHED building fluent-bit plugin"
 
-# Write-Output "building promconfigvalidator"
+Write-Output "building promconfigvalidator"
 
-# Set-Location ..
-# Set-Location prom-config-validator-builder
+Set-Location ..
+Set-Location prom-config-validator-builder
 
-# .\makefile_windows.ps1
+.\makefile_windows.ps1
 
-# Set-Location ..
-# Set-Location opentelemetry-collector-builder
+Set-Location ..
+Set-Location opentelemetry-collector-builder
 
-# Write-Output "FINISHED building promconfigvalidator"
+Write-Output "FINISHED building promconfigvalidator"
 
 Set-Location ..
 Set-Location main
