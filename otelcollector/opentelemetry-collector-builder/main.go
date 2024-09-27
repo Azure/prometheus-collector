a55a335929023cd465c77823e6ff7cd17c6c76b7
Grace Wehner <grace.wehner@microsoft.com>
2024-09-26 18:46:02 -0700

    use fileprovider

--- otelcollector/opentelemetry-collector-builder/main.go
+++ otelcollector/opentelemetry-collector-builder/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/confmap/provider/envprovider"
+	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
 	"go.opentelemetry.io/collector/otelcol"
 )
 
@@ -14,7 +17,20 @@ func main() {
 		Version:     "0.109.0",
 	}
 
-	app := otelcol.NewCommand(otelcol.CollectorSettings{BuildInfo: info, Factories: components})
+	set := otelcol.CollectorSettings{
+		BuildInfo: info,
+		Factories: components,
+		ConfigProviderSettings: otelcol.ConfigProviderSettings{
+			ResolverSettings: confmap.ResolverSettings{
+				ProviderFactories: []confmap.ProviderFactory{
+					envprovider.NewFactory(),
+					fileprovider.NewFactory(),
+				},
+			},
+		},
+	}
+
+	app := otelcol.NewCommand(set)
 	err := app.Execute()
 	if err != nil {
 		log.Fatal("collector server run finished with error: %w", err)
