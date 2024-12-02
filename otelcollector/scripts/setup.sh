890a1ad61b77a4d91546d7a620aeaf9aa7f9fc00
Grace Wehner <grace.wehner@microsoft.com>
2024-12-02 13:52:14 -0800

    support mariner

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -58,7 +58,8 @@ sudo tdnf list installed | grep telegraf | awk '{print $2}' > telegrafversion.tx
 # Install fluent-bit
 # echo "Installing fluent-bit..."
 # sudo tdnf install fluent-bit-2.1.10 -y
-curl https://raw.githubusercontent.com/fluent/fluent-bit/master/install.sh | sh
+curl https://packages.fluentbit.io/centos/7/fluent-bit-3.2.2-1.x86_64.rpm
+sudo tdnf install -y fluent-bit-3.2.2-1.x86_64.rpm
 
 # Setup hourly cron for logrotate
 cp /etc/cron.daily/logrotate /etc/cron.hourly/
