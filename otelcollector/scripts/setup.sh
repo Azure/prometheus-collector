a32b4bebb819e063031c514637eca50de5efb92f
Grace Wehner <grace.wehner@microsoft.com>
2024-12-10 12:48:51 -0800

    add cm2

--- otelcollector/scripts/setup.sh
+++ otelcollector/scripts/setup.sh
@@ -52,7 +52,7 @@ mkdir /opt/microsoft/linuxmonagent
 
 # Install telegraf
 echo "Installing telegraf..."
-sudo tdnf install telegraf-1.29.4-6 -y
+sudo tdnf install telegraf-1.29.4-6.cm2 -y
 sudo tdnf list installed | grep telegraf | awk '{print $2}' > telegrafversion.txt
 
 # Install fluent-bit
