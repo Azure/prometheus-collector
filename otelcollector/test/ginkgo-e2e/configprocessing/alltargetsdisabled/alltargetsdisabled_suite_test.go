0fc791815331b5ca674344266246c42ebc40c18c
Rashmi Chandrashekar <rashmy@microsoft.com>
2025-03-07 09:47:21 -0800

    creating suite

---
+++ otelcollector/test/ginkgo-e2e/configprocessing/alltargetsdisabled/alltargetsdisabled_suite_test.go
@@ -0,0 +1,13 @@
+package configprocessingalltargetsdisabled_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestAlltargetsdisabled(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Alltargetsdisabled Suite")
+}
