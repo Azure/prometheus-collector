56b5a1b2f6522e7d567bf42aa510df97be05fdf5
Tina Ngo <ngo.t.christina@gmail.com>
2025-03-12 13:53:46 -0400

    minor fixes

--- otelcollector/scripts/ccpsetup.sh
+++ otelcollector/scripts/ccpsetup.sh
@@ -35,7 +35,7 @@ mkdir /opt/microsoft/linuxmonagent
 
 # Install ME
 echo "Installing Metrics Extension..."
-sudo tdnf install -y metricsext2
+sudo tdnf install -y metricsext2-2.2025.307.1609
 sudo tdnf list installed | grep metricsext2 | awk '{print $2}' > metricsextversion.txt
 
 # Remove any RPMs downloaded not from Mariner
