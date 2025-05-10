c14a88a9ad3497cbc7ba75d7718eb861ba03d126
bragi92 <kadubey@microsoft.com>
2025-05-10 15:23:31 -0700

    fix: move first hash call to after MA, ME startup (#1149)
    [comment]: # (Note that your PR title should follow the conventional
    commit format: https://conventionalcommits.org/en/v1.0.0/#summary)
    # PR Description
    
    [comment]: # (The below checklist is for PRs adding new features. If a
    box is not checked, add a reason why it's not needed.)
    # New Feature Checklist
    
    - [ ] List telemetry added about the feature.
    - [ ] Link to the one-pager about the feature.
    - [ ] List any tasks necessary for release (3P docs, AKS RP chart
    changes, etc.) after merging the PR.
    - [ ] Attach results of scale and perf testing.
    
    [comment]: # (The below checklist is for code changes. Not all boxes
    necessarily need to be checked. Build, doc, and template changes do not
    need to fill out the checklist.)
    # Tests Checklist
    
    - [ ] Have end-to-end Ginkgo tests been run on your cluster and passed?
    To bootstrap your cluster to run the tests, follow [these
    instructions](/otelcollector/test/README.md#bootstrap-a-dev-cluster-to-run-ginkgo-tests).
      - Labels used when running the tests on your cluster:
        - [ ] `operator`
        - [ ] `windows`
        - [ ] `arm64`
        - [ ] `arc-extension`
        - [ ] `fips`
    - [ ] Have new tests been added? For features, have tests been added for
    this feature? For fixes, is there a test that could have caught this
    issue and could validate that the fix works?
      - [ ] Is a new scrape job needed?
    - [ ] The scrape job was added to the folder
    [test-cluster-yamls](/otelcollector/test/test-cluster-yamls/) in the
    correct configmap or as a CR.
      - [ ] Was a new test label added?
    - [ ] A string constant for the label was added to
    [constants.go](/otelcollector/test/utils/constants.go).
    - [ ] The label and description was added to the [test
    README](/otelcollector/test/README.md).
    - [ ] The label was added to this [PR
    checklist](/.github/pull_request_template).
    - [ ] The label was added as needed to
    [testkube-test-crs.yaml](/otelcollector/test/testkube/testkube-test-crs.yaml).
      - [ ] Are additional API server permissions needed for the new tests?
    - [ ] These permissions have been added to
    [api-server-permissions.yaml](/otelcollector/test/testkube/api-server-permissions.yaml).
      - [ ] Was a new test suite (a new folder under `/tests`) added?
    - [ ] The new test suite is included in
    [testkube-test-crs.yaml](/otelcollector/test/testkube/testkube-test-crs.yaml).

--- otelcollector/shared/filesystemwatcher_utility.go
+++ otelcollector/shared/filesystemwatcher_utility.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 )
@@ -21,57 +22,88 @@ func CheckForFilesystemChanges() {
 	tokenFile := `C:\opt\genevamonitoringagent\datadirectory\mcs\metricsextension\TokenConfig.json`
 	hashStore := `C:\last_config_hash.txt`
 	logFile := `C:\filesystemwatcher.txt`
+	debugLog := `C:\filesystemwatcher_error_log.txt`
 
-	h := sha256.New()
+	debug := func(format string, a ...any) {
+		msg := fmt.Sprintf(format, a...)
+		msg = time.Now().Format("2006-01-02 15:04:05") + " " + msg + "\n"
 
-	tokenFileExists := false
-	if info, err := os.Stat(tokenFile); err == nil && !info.IsDir() {
-		tokenFileExists = true
+		f, err := os.OpenFile(debugLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			// Fallback to stdout if file can't be written
+			fmt.Printf("Failed to write debug log: %v\n", err)
+			return
+		}
+		defer f.Close()
+
+		f.WriteString(msg)
 	}
 
-	// Hash all files in initial paths
+	h := sha256.New()
+	var allFiles []string
+
+	// Helper function to check for Kubernetes symlinks
+	isK8sSymlink := func(name string) bool {
+		return strings.HasPrefix(name, "..")
+	}
+
+	// Collect all files from initial paths
 	for _, dir := range initialPaths {
 		filepath.WalkDir(dir, func(path string, d os.DirEntry, err error) error {
-			if err != nil || d.IsDir() {
+			if err != nil {
+				debug("Skipping %s due to error: %v", path, err)
 				return nil
 			}
-			return hashFileContents(h, path)
+			// Skip directories and symlinks like `..data` or `..202x_*`
+			if d.IsDir() || isK8sSymlink(d.Name()) {
+				return nil
+			}
+			allFiles = append(allFiles, path)
+			return nil
 		})
 	}
 
-	// If TokenConfig.json exists, hash it
-	if tokenFileExists {
-		hashFileContents(h, tokenFile)
+	// If TokenConfig.json exists, add it to the list
+	if info, err := os.Stat(tokenFile); err == nil && !info.IsDir() {
+		allFiles = append(allFiles, tokenFile)
+	} else {
+		debug("Token file not found or is a directory: %v", err)
 	}
 
-	// Generate final combined hash
+	// Sort the list to ensure deterministic hash input
+	sort.Strings(allFiles)
+
+	// Hash file paths and contents
+	for _, path := range allFiles {
+		if err := hashFileContents(h, path); err != nil {
+			debug("Failed to hash file %s: %v", path, err)
+		}
+	}
+
+	// Generate final hash
 	finalHash := hex.EncodeToString(h.Sum(nil))
 
-	// Compare to last stored hash
-	lastHashBytes, _ := os.ReadFile(hashStore)
-	lastHash := string(lastHashBytes)
-	lastHash = strings.TrimSpace(lastHash)
+	// Compare with last stored hash
+	lastHashBytes, err := os.ReadFile(hashStore)
+	lastHash := ""
+	if err != nil {
+		debug("Could not read last hash from %s: %v", hashStore, err)
+	} else {
+		lastHash = strings.TrimSpace(string(lastHashBytes))
+	}
 
 	if lastHash == "" {
-		// On first run, just write the final hash to the file so that we have a value to compare against
 		os.WriteFile(hashStore, []byte(finalHash), 0644)
-	} else {
-		// Check if the hash has changed
-		if finalHash != lastHash {
-			// Update stored hash only if it changed
-			os.WriteFile(hashStore, []byte(finalHash), 0644)
+	} else if finalHash != lastHash {
+		os.WriteFile(hashStore, []byte(finalHash), 0644)
+		now := time.Now().Format(time.RFC3339)
+		msg := fmt.Sprintf("Configuration changed at %s\n", now)
 
-			// Log the change (only when the hash has changed)
-			now := time.Now().Format(time.RFC3339)
-			msg := fmt.Sprintf("Configuration changed at %s\n", now)
-
-			f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err == nil {
-				defer f.Close()
-				f.WriteString(msg)
-			} else {
-				fmt.Println("Failed to write to log file:", err)
-			}
+		if f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err == nil {
+			defer f.Close()
+			f.WriteString(msg)
+		} else {
+			debug("Failed to write to log file: %v", err)
 		}
 	}
 }
@@ -79,11 +111,14 @@ func CheckForFilesystemChanges() {
 func hashFileContents(h hash.Hash, path string) error {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil
+		return fmt.Errorf("unable to open file %s: %w", path, err)
 	}
 	defer f.Close()
 
-	io.WriteString(h, path) // Include path in hash for stability
+	io.WriteString(h, path) // Include path in hash
 	_, err = io.Copy(h, f)
-	return err
+	if err != nil {
+		return fmt.Errorf("error reading file %s: %w", path, err)
+	}
+	return nil
 }
