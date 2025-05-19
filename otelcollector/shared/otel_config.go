714d6bc4bb7f8a97d3e9dc98851a984fd4588fe2
Rashmi Chandrashekar <rashmy@microsoft.com>
2025-05-19 15:48:33 -0700

    adding omit empty

--- otelcollector/shared/otel_config.go
+++ otelcollector/shared/otel_config.go
@@ -36,7 +36,7 @@ type OtelConfig struct {
 	Receivers  struct {
 		Prometheus struct {
 			Config          map[string]interface{} `yaml:"config"`
-			TargetAllocator map[string]interface{} `yaml:"target_allocator"`
+			TargetAllocator map[string]interface{} `yaml:"target_allocator,omitempty"`
 			APIServer       interface{}            `yaml:"api_server"`
 		} `yaml:"prometheus"`
 	} `yaml:"receivers"`
