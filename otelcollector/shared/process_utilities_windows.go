a53e69414ba67eed96806afd9db5699231acd1bf
Grace Wehner <grace.wehner@microsoft.com>
2025-06-03 17:12:43 -0700

    fix port

--- otelcollector/shared/process_utilities_windows.go
+++ otelcollector/shared/process_utilities_windows.go
@@ -310,7 +310,7 @@ func StartMetricsExtensionForOverlay(meConfigFile string, meDCRConfigDirectory s
 		if meLocalControl {
 			cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-LocalControlChannel", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom", "-ConfigOverridesFilePath", meConfigFile)
 		} else {
-			cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom,otlp_grpc,otlp_http", "-OtlpHttpHost", "0.0.0.0", "-OtlpHttpPort", "4320", "-ConfigOverridesFilePath", meConfigFile)
+			cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom,otlp_grpc,otlp_http", "-OtlpHttpHost", "0.0.0.0", "-OtlpHttpPort", "56680", "-ConfigOverridesFilePath", meConfigFile)
 		}
 
 	case "windows":
@@ -356,7 +356,7 @@ func StartMetricsExtensionWithConfigOverridesForUnderlay(configOverrides string,
 	if meLocalControl {
 		cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-LocalControlChannel", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom", "-ConfigOverridesFilePath", "/usr/sbin/me.config")
 	} else {
-		cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom,otlp_grpc,otlp_http", "-OtlpHttpHost", "0.0.0.0", "-OtlpHttpPort", "4320", "-ConfigOverridesFilePath", "/usr/sbin/me.config")
+		cmd = exec.Command("/usr/sbin/MetricsExtension", "-Logger", "File", "-LogLevel", "Info", "-TokenSource", "AMCS", "-DataDirectory", meDCRConfigDirectory, "-Input", "otlp_grpc_prom,otlp_grpc,otlp_http", "-OtlpHttpHost", "0.0.0.0", "-OtlpHttpPort", "56680", "-ConfigOverridesFilePath", "/usr/sbin/me.config")
 	}
 	// Create a file to store the stdoutput
 	// metricsextension_stdout_file, err := os.Create("metricsextension_stdout.log")
