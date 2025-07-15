70ffea278d6456ea9fd276225567b497040402cc
Rashmi Chandrashekar <rashmy@microsoft.com>
2025-07-15 15:47:22 -0700

    add error logging

--- otelcollector/otel-allocator/internal/config/flags.go
+++ otelcollector/otel-allocator/internal/config/flags.go
@@ -4,7 +4,6 @@
 package config
 
 import (
-	uberzap "go.uber.org/zap"
 	"flag"
 
 	"github.com/spf13/pflag"
@@ -41,8 +40,8 @@ func getFlagSet(errorHandling pflag.ErrorHandling) *pflag.FlagSet {
 	flagSet.String(httpsTLSKeyFilePathFlagName, "", "The path to the HTTPS server TLS key file.")
 	zapFlagSet := flag.NewFlagSet("", flag.ErrorHandling(errorHandling))
 	zapCmdLineOpts.BindFlags(zapFlagSet)
-	lvl := uberzap.NewAtomicLevelAt(uberzap.PanicLevel)
-	zapCmdLineOpts.Level = &lvl
+	//lvl := uberzap.NewAtomicLevelAt(uberzap.PanicLevel)
+	//zapCmdLineOpts.Level = &lvl
 	flagSet.AddGoFlagSet(zapFlagSet)
 	return flagSet
 }
