5ad8070392d9233aa8785686dce5166607f13854
Rashmi Chandrashekar <rashmy@microsoft.com>
2026-04-09 11:55:10 -0700

    removing helper method

--- otelcollector/configuration-reader-builder/main.go
+++ otelcollector/configuration-reader-builder/main.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
-	"strconv"
 	"strings"
 	"time"
 
@@ -25,6 +24,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilversion "k8s.io/apimachinery/pkg/util/version"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -136,7 +136,11 @@ func updateTAConfigFile(configFilePath string, httpsEnabled bool) {
 	log.Println("Defaulting to watch all namespaces for secrets")
 
 	kubeVersion := os.Getenv("KUBE_VERSION")
-	if kubeVersion != "" && !isKubeVersionLessThan(kubeVersion, 1, 36) {
+	parsedKubeVersion, kubeVersionErr := utilversion.ParseSemantic(kubeVersion)
+	if kubeVersion != "" && kubeVersionErr != nil {
+		log.Printf("Failed to parse Kubernetes version %q: %v, defaulting to watch all namespaces\n", kubeVersion, kubeVersionErr)
+	}
+	if kubeVersion != "" && kubeVersionErr == nil && !parsedKubeVersion.LessThan(utilversion.MustParseSemantic("v1.36.0")) {
 		// On >= 1.36, read the scoped namespace list from the configmap setting.
 		secretsAccessNamespaces = nil
 		if sns := os.Getenv("AZMON_SECRETS_ACCESS_NAMESPACES"); sns != "" {
@@ -150,7 +154,7 @@ func updateTAConfigFile(configFilePath string, httpsEnabled bool) {
 		} else {
 			log.Printf("Kubernetes version %s >= 1.36: no secrets_access_namespaces configured, no namespaces will be watched for secrets\n", kubeVersion)
 		}
-	} else if kubeVersion != "" {
+	} else if kubeVersion != "" && kubeVersionErr == nil {
 		log.Printf("Kubernetes version %s < 1.36: watching all namespaces for secrets\n", kubeVersion)
 	}
 
@@ -650,26 +654,4 @@ func main() {
 
 	http.ListenAndServe(":8081", nil)
 
-}
-
-// isKubeVersionLessThan parses a Kubernetes version string (e.g. "1.30.1", "v1.35.0")
-// and returns true if it is strictly less than major.minor.
-func isKubeVersionLessThan(version string, major, minor int) bool {
-	v := strings.TrimPrefix(version, "v")
-	parts := strings.SplitN(v, ".", 3)
-	if len(parts) < 2 {
-		return true
-	}
-	parsedMajor, err := strconv.Atoi(parts[0])
-	if err != nil {
-		return true
-	}
-	parsedMinor, err := strconv.Atoi(parts[1])
-	if err != nil {
-		return true
-	}
-	if parsedMajor != major {
-		return parsedMajor < major
-	}
-	return parsedMinor < minor
 }
